0d4dd97d29fbfd7cbd6236025f46aef0d396a1d5
Oleksandr Shmatko <shmatko1095@gmail.com>
2019-03-21 22:29:34 +0200

    rand() function add
    Signed-off-by: Oleksandr Shmatko <shmatko1095@gmail.com>

--- 01-Git_Bash_Make/randomGame.c
+++ 01-Git_Bash_Make/randomGame.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+//#include <ctime.h>
 
 int main(){
   int x;
   printf("your number: ");
   scanf("%d",&x);
   printf("your number is: %d \r\n", x);
+
+  srand ( time(NULL) );
+  int random = rand() % 10 + 1;
+  printf("random number is: %d \r\n", random);
+
   return 0;
 }
