2898ee0e469e174b0617f648b66b21d6ee85f599
Maxim Primerov <primerovmax@gmail.com>
2019-03-21 17:10:14 +0200

    Lesson 2 Part 1: Add game output
    Add generate random number
    Add comparing user number and random number
    Add game output
    
    Signed-off-by: Maxim Primerov <primerovmax@gmail.com>

--- 01-Git_Bash_Make/main.c
+++ 01-Git_Bash_Make/main.c
@@ -8,6 +8,12 @@ int main()
         for (int i = 0; i < 10; ++i) {
                 printf("\nInsert the number -> ");
                 scanf("%d", &number);
+
+                if (rand() % 10 == number) {
+                        printf("You win!");
+                } else {
+                        printf("You loose!");
+                }
         }
 
         return 0;
