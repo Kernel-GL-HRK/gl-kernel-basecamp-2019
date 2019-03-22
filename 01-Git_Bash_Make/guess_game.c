66769edd453daa7aa27fc37cc2a5c0bbf2419733
Dima Moruha <trluxsus@gmail.com>
2019-03-22 18:02:57 +0200

    Homework lesson 2
    Add AI nuber generator
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

--- 01-Git_Bash_Make/guess_game.c
+++ 01-Git_Bash_Make/guess_game.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
         int input = 0;
+        int ai = rand()%10;
 
         printf("Input number from 0 to 9\n");
         scanf("%d",&input);
