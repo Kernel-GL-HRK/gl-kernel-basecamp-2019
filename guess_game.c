d33d1efa26d3e53c2675613edab853855d2cbe05
Dima Moruha <trluxsus@gmail.com>
2019-03-22 15:05:35 +0200

    Add ai number generator
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

--- guess_game.c
+++ guess_game.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     int input = 0;
+    int ai = rand()%10;
 
     printf("Input number from 0 to 9\n");
     scanf("%d",&input);
-    
+
     return 0;
 }
