2fd54094867b1499e13ba05439afea30e6bc9a5f
Daniil Petrov <daaaanil81@gmail.com>
2019-03-22 13:16:39 +0200

    Lesson 2 Part 1: Add game mechanic
    Signed-off-by: Daniil Petrov <daaaanil81@gmail.com>

--- 01-Git_Bash_Make/homework01.c
+++ 01-Git_Bash_Make/homework01.c
@@ -7,9 +7,18 @@ int main()
     srand(time(NULL));
     int computer = rand() % 10;
     int user = 0;
-    while(user != -1)
-    {
-        break;
+    while(0 < user || user > 9) {
+        printf("Pleace, Enter number 0-9: \n");
+        printf("For exit, enter number: -1");
+        scanf("%d", &user);
+        if (user == computer)
+        {
+            printf("You Win!!!");
+        }
+        else
+        {
+            printf("You Lose!!!");
+        }
     }
-	return 0;
+    return 0;
 }
