12b12854128fb26b4f75ae6504d5623d81844b51
Kirill Kravtsov <nekravtsovkirill@gmail.com>
2019-03-22 22:23:35 +0200

    Add condition for number checks.
    Signed-off-by: Kirill Kravtsov <nekravtsovkirill@gmail.com>

--- 01-Git_Bash_Make/HW1.cpp
+++ 01-Git_Bash_Make/HW1.cpp
@@ -2,5 +2,19 @@
 
 int main()
 { 
+    int value = 4, input;
+
+    std::cout << "Please input number to play a lottery!\n" << std:: endl;
+    std::cin >> input;
+
+    if (value == input)
+    {
+        std::cout << "You won!"<< std::endl;
+        break;
+    }
+    else
+    {
+        std::cout << "You lose!"<< std::endl;
+    }    
     return 1;
 }
