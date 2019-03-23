e68b41005726e275186ebba4420cd94cdfb31488
Kulichenko Viacheslav <Kulichenko.Viacheslav@gmail.com>
2019-03-23 21:46:21 +0200

    Lesson 2 Part 1: Add check and output of result message
    Signed-off-by: Kulichenko Viacheslav <Kulichenko.Viacheslav@gmail.com>

--- Task1/main.c
+++ Task1/main.c
@@ -3,11 +3,15 @@
 
 int main()
 {
-    unsigned int user_number = 0;
-    unsigned int rand_number = 0;
-    printf("Enter any number from 0 to 9 >");
-    scanf("%i", &user_number);
-    srand(time(NULL));
-    rand_number = rand() % 10;
-    return 0;
+        unsigned int user_number = 0;
+        unsigned int rand_number = 0;
+        printf("Enter any number from 0 to 9 >");
+        scanf("%i", &user_number);
+        srand(time(NULL));
+        rand_number = rand() % 10;
+        if (user_number == rand_number)
+                printf("You win :)\r\n");
+        else
+                printf("You loose :(\r\n");
+        return 0;
 }
