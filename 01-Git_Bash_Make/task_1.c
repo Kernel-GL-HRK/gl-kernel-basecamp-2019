d61315e8942762c7ad918ba17b8dbae5173c5514
Eduard Voronkin <eduard.voronkin@nure.ua>
2019-03-24 00:19:30 +0200

    Lesson 2 Part 1: Remove unnecessary brackets

--- 01-Git_Bash_Make/task_1.c
+++ 01-Git_Bash_Make/task_1.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h> //for rand()
-int main(){
+#include <time.h>
+int main()
+{
     int usr_input;
+    int random_number = rand() % 10;
+    for(int i = 0; i < 100; ++i){
     printf("Enter a number");
     scanf("%d", &usr_input);
-    int random_number = rand() % 10;
     if(usr_input == random_number)
         printf("You win\n");
     else
