b0d1564435f0adcab826f9ee3fed69d7b01bda7c
Ihor Ryzhov <ihor.ryzhov@nure.ua>
2019-03-23 19:51:47 +0200

    Lesson 2 Part 1: Add random number generator

--- 01-Git_Bash_Make/Task1_game/main.c
+++ 01-Git_Bash_Make/Task1_game/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void Game(void)
+{
+    uint32_t number;
+    srand(time(0));
+
+    scanf("%u", &number);
+
+    if(number == (rand() % 10))
+    {
+        printf("You win! \n");
+    }
+    else
+    {
+        printf("You loose! \n");
+    }
+}
 
 int main()
 {
+    Game();
     return 0;
 }
