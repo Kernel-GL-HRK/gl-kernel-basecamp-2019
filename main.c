5290ba126f2b73073190fbe82a935645235efe5f
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-03-23 22:27:02 +0200

    Second commit add main function
    Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- main.c
+++ main.c
@@ -2,3 +2,18 @@
 #include<stdio.h>
 #include<time.h>
 using namespace std;
+
+int main(int argc, char** argv) {
+int PlayerVallue;
+while(1){
+printf ("ВВедите Ваше число\n");
+scanf("%d",&PlayerVallue);
+srand(time(NULL));
+int AiVallue = rand() %10 +1;
+
+printf("%d\n", AiVallue);
+}
+
+
+
+}
