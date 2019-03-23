4e82dec906d93acace2bce63e022c2511533fe9a
Mark Rubinshteyn <mark.rubinshteyn@meta.ua>
2019-03-23 09:25:35 +0200

    Lesson 2 part 4:Add win condition
    Add if bracets and win condition.Fix code style
    
    Signed-off-by: Mark Rubinshteyn <mark.rubinshteyn@meta.ua>

--- 01-Git_Bash_Make/main.c
+++ 01-Git_Bash_Make/main.c
@@ -3,11 +3,17 @@
 
 using namespace std ;
 
-int main()
+int main(void)
 {
-	int a,b;
-	cout <<"Please insert number beetwen 0 and 9\n";
-	cin>>a;
-	b=random()%10;
+	int a, b;
+
+	cout < < "Please insert number beetwen 0 and 9\n";
+	cin >> a;
+	b = random()%10;
+	if (a == b) {
+	    cout << "you won\n";
+
+} else
+cout << "you lose\n";
 	return 0;
 }
