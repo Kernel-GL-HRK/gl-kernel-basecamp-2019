6e4d8200ef4604c38713115fac5c5ccbd977c66f
Kirill Kravtsov <nekravtsovkirill@gmail.com>
2019-03-23 10:49:12 +0200

    Lesson 2 Part 1: Add 'if' condition
    Signed-off-by: Kirill Kravtsov <nekravtsovkirill@gmail.com>

--- 01-Git_Bash_Make/HW1.cpp
+++ 01-Git_Bash_Make/HW1.cpp
@@ -2,5 +2,12 @@
 
 int main()
 { 
+    int value = 4;
+
+    if (value == rand()%10) {
+        std::cout << "You won!"<< std::endl;
+    } else {
+        std::cout << "You lose!"<< std::endl;
+    }
     return 1;
 }
