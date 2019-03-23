5649933da9ebc228f6521cf94edc06025cb8f1dc
Maxim Primerov <primerovmax@gmail.com>
2019-03-23 23:56:13 +0200

    Lesson 3 part 2: Add copying file main.c
    - file availability check
    - copy file
    
    Signed-off-by: Maxim Primerov <primerovmax@gmail.com>

--- 01-Git_Bash_Make/script.sh
+++ 01-Git_Bash_Make/script.sh
@@ -6,3 +6,12 @@ if ! [ -d /tmp/guesanumber ]
 then
         mkdir -v /tmp/guesanumber # Create directory
 fi
+
+# File check
+if ! [ -f /tmp/guesanumber/main.c ]
+then
+        cp -v main.c /tmp/guesanumber # Copy file
+else
+        rm -v /tmp/guesanumber/main.c # Remove old file
+        cp -v main.c /tmp/guesanumber # Copy file
+fi
