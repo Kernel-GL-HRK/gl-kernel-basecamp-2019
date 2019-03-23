33b57aebcc174780523b5a07bf58b00ffb62ccee
Kirill Kravtsov <nekravtsovkirill@gmail.com>
2019-03-23 10:52:13 +0200

    Lesson 2 Part 1: Add input hints for user
    Signed-off-by: Kirill Kravtsov <nekravtsovkirill@gmail.com>

--- 01-Git_Bash_Make/HW1.cpp
+++ 01-Git_Bash_Make/HW1.cpp
@@ -2,7 +2,9 @@
 
 int main()
 { 
-    int value = 4;
+    int value = 4, input;
+    std::cout << "Please input number to play a lottery!\n" << std:: endl;
+    std::cin >> input;
 
     if (value == rand()%10) {
         std::cout << "You won!"<< std::endl;
