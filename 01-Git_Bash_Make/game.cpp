d90a7b7a71385d84fd5315842b6e6fc76142d4eb
Anokhina Irina <irina15anohina@gmail.com>
2019-03-23 16:52:39 +0200

    Lesson 2 Part 1: Add request to user
    Signed-off-by: Anokhina Irina <irina15anohina@gmail.com>

--- 01-Git_Bash_Make/game.cpp
+++ 01-Git_Bash_Make/game.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
-
+#include<stdint.h>
 int main()
 {
-std::cout << "Now I`m empty\n";
+uint8_t num;
+std::cout << "Please, enter some number from 0 to 9:\n";
+std::cin >> num;
+
 
 return 0;
 }
