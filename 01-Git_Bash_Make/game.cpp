e416cd090dd9eab3f964dc6694b6b409418d8701
Anokhina Irina <irina15anohina@gmail.com>
2019-03-23 17:53:51 +0200

    Lesson 2 Part 1: Add randomization and validation check out
    Signed-off-by: Anokhina Irina <irina15anohina@gmail.com>

--- 01-Git_Bash_Make/game.cpp
+++ 01-Git_Bash_Make/game.cpp
@@ -1,11 +1,30 @@
 #include<iostream>
+using std::cout;
+using std::cin;
 #include<stdint.h>
+#include<ctime>
+#include<stdlib.h>
+
 int main()
 {
-uint8_t num;
-std::cout << "Please, enter some number from 0 to 9:\n";
-std::cin >> num;
-
-
+int num;
+char answer = 'y';
+while(answer == 'y')
+{
+cout << "Please, enter some number from 0 to 9:\n";
+cin >> num;
+while(num > 9 || num < 0)
+{
+cout << "You entered wrong number\n"
+<< " Please, enter number from 0 to 9:\n";
+cin >> num;
+}
+srand(time(0));		//auto randomisation
+int randNum = rand() % 10;
+cout << "Your number is: " << num <<" and my number is: " << randNum;
+cout << "\nIf want to continue, press 'y', if you want to exit, press another"
+<< " character\n";
+cin >> answer;
+}
 return 0;
 }
