4d115d573aaa374609156ba292f947cba0195307
Anokhina Irina <irina15anohina@gmail.com>
2019-03-24 10:50:23 +0200

    Lesson 2 Part 1: Change library
    Signed-off-by: Anokhina Irina <irina15anohina@gmail.com>

--- 01-Git_Bash_Make/game.cpp
+++ 01-Git_Bash_Make/game.cpp
@@ -3,25 +3,27 @@ using std::cout;
 using std::cin;
 #include<stdint.h>
 #include<ctime>
-#include<stdlib.h>
+#include<cstdlib>
 
 int main()
 {
 int num;
 char answer = 'y';
-while(answer == 'y')
-{
+while(answer == 'y') {
 cout << "Please, enter some number from 0 to 9:\n";
 cin >> num;
-while(num > 9 || num < 0)
-{
+while(num > 9 || num < 0) {
 cout << "You entered wrong number\n"
 << " Please, enter number from 0 to 9:\n";
 cin >> num;
 }
 srand(time(0));		//auto randomisation
 int randNum = rand() % 10;
-cout << "Your number is: " << num <<" and my number is: " << randNum;
+if (randNum == num)
+	cout << "You win\n";
+else
+	cout << "You lose\n";
+
 cout << "\nIf want to continue, press 'y', if you want to exit, press another"
 << " character\n";
 cin >> answer;
