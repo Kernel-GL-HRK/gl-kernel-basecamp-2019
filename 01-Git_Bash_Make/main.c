f2da6d4d5d191da194417d406faaa2d9454b4230
Daniel Shami <danismi715@gmail.com>
2019-03-24 16:34:06 +0200

    Git Lesson Part1: Add function to get random number
    Signed-off-by: Daniel Shami <danismi715@gmail.com>

--- 01-Git_Bash_Make/main.c
+++ 01-Git_Bash_Make/main.c
@@ -6,8 +6,11 @@ int main()
 {
     srand(time(NULL));
     int random_number = rand() % 10;
+<<<<<<< HEAD
 
     int userNumber;
     scanf("%d",&userNumber);
+=======
+>>>>>>> 00c36c1... Git_Lesson_v0.0.2
     return 0;
 }
