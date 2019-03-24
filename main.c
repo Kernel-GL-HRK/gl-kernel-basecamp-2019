449214a3b872ed817a06eb14fc6aeb870f172a19
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-03-24 16:47:44 +0200

    Lesson 2 part 1: Add main function Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- main.c
+++ main.c
@@ -3,3 +3,11 @@
 #include <time.h>
 using namespace std;
 
+int main(int argc, char** argv) {
+    int PlayerVallue;
+        while(1) {
+            printf ("Введите Ваше число\n");
+                scanf("%d",&PlayerVallue);
+                    srand(time(NULL));
+                        int AiVallue = rand() % 10 +1;
+                        printf ("%d \n", AiVallue);
