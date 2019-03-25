85c5968ff936459b7cc41a60887ff97611ceab89
Oleksandr.Korotetskyi <ayrum1158@gmail.com>
2019-03-25 15:04:52 +0200

    LEsson 02 Part 01: Add "RNum" variable randomisation
    Signed-off-by: Oleksandr.Korotetskyi <ayrum1158@gmail.com>

--- 01-Git_Bash_Make/guessanumber.c
+++ 01-Git_Bash_Make/guessanumber.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <time.h>
 
 void main()
 {
+	srand(unsigned(time(NULL)));
+
 	int RNum, UsrNum;
 
 	printf(" Enter your number:\t");
 	scanf("%i", &UsrNum);
+
+	RNum = rand()%10;
 
 
 
