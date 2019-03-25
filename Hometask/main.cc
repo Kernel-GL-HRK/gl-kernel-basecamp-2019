b0502846b0e6cc5905d83884955dc6012de93c35
Dmytro Kalynenko <dmytrokalynenko@gmail.com>
2019-03-25 11:00:03 +0200

    Task 1: Second commit
    Add function InputData. This function allows the user to enter new data.
    
    Signed-off-by: Dmytro Kalynenko <dmytrokalynenko@gmail.com>

--- Hometask/main.cc
+++ Hometask/main.cc
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+void InputDaTa(int &k)
+{
+	printf("Please Enter number beetwens 0 to 9:");
+	scanf_s("%d", &k);
+}
+
+
 int main()
 {
+        int numbers;
+	Input(numbers);
 	return 0;
 }
