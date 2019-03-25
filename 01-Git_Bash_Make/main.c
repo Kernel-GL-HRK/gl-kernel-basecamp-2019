8295effb5e09df405e78ddbe50737f3c0f558dfa
Volodymyr Mietielov <vometel@gmail.com>
2019-03-25 13:44:27 +0200

    Add while cycle
    Implemented while cycle
    
    Signed-off-by: Volodymyr Mietielov <vometel@gmail.com>

--- 01-Git_Bash_Make/main.c
+++ 01-Git_Bash_Make/main.c
@@ -10,4 +10,15 @@ int main(void)
     srand(time(NULL));
     n = 1 + (rand() % 9);
 
+    puts("I'm thinking of a number between 1 and 9.");
+    puts("Try to guess it:");
+
+    while (1) {
+        if (scanf("%d", &g) != 1) {
+		/* ignore one char, in case user gave a non-number */
+		scanf("%c", &c);
+		continue;
+	}
+
+    }
 }
