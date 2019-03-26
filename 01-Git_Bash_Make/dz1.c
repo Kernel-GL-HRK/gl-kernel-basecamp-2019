b872721eb77f2968232badd205c1bb8d52a77050
Maxim Lipchanskyi <m4554747@gmail.com>
2019-03-27 00:54:38 +0200

    Lesson 2 Part 1: Add main() function implementation
    Added implementation of base algorithm in main() function
    
    Signed-off-by: Maxim Lipchanskyi <m4554747@gmail.com>

--- 01-Git_Bash_Make/dz1.c
+++ 01-Git_Bash_Make/dz1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) 
 {
-
-    return 0;
+        int d;
+        int r;
+    
+        srand(time(NULL));
+        printf("Enter number 0-9>");
+        scanf("%d", &d);
+        r = rand() % 10;
+        if (d == r) {
+                printf("You win\n");
+                return 0;
+        }
+        else {
+                printf("You loose\n");
+                return 1;
+        }
+    
+        return 0;
 }
-
