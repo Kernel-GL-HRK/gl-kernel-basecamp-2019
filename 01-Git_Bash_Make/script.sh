d287547a2738e90edfbb6a328fb3970b6ef81b24
Kirill Kravtsov <nekravtsovkirill@gmail.com>
2019-03-27 18:36:21 +0200

    Lesson 3 Part 2: Add copying archive to realese dir
    Signed-off-by: Kirill Kravtsov <nekravtsovkirill@gmail.com>

--- 01-Git_Bash_Make/script.sh
+++ 01-Git_Bash_Make/script.sh
@@ -13,3 +13,14 @@ cp ./*.* $DISTDIR
 cd /tmp/
 tar -czvf guesanumber.tar.gz ./guesanumber
 
+if [ ! -d $DISTDIR/realese ]
+    then 
+    mkdir $DISTDIR/realese
+elif [ ! -z "$(ls -A $DISTDIR/realese)" ]
+    then
+    rm $DISTDIR/realese/*.*
+else
+    cp guesanumber.tar.gz $DISTDIR/realese/
+    rm $DISTDIR/*.*
+fi
+
