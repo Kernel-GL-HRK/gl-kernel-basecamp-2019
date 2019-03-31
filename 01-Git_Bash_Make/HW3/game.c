0b2931130045c4c1cbac65c5338ed5b165ad2310
Anokhina Irina <irina15anohina@gmail.com>
2019-03-31 08:53:01 +0300

    Lesson 2-4 Part 3: Split one source file in three files
    Signed-off-by: Anokhina Irina <irina15anohina@gmail.com>

---
+++ 01-Git_Bash_Make/HW3/game.c
@@ -0,0 +1,26 @@
+#include "game.h"
+
+void game()
+{
+int num;
+char answer = 'y';
+while(answer == 'y') {
+cout << "Please, enter some number from 0 to 9:\n";
+cin >> num;
+while(num > 9 || num < 0) {
+cout << "You entered wrong number\n"
+<< " Please, enter number from 0 to 9:\n";
+cin >> num;
+}
+srand(time(0));		//auto randomisation
+int randNum = rand() % 10;
+if (randNum == num)
+	cout << "You win\n";
+else
+	cout << "You lose\n";
+
+cout << "\nIf want to continue, press 'y', if you want to exit, press another"
+<< " character\n";
+cin >> answer;
+}
+}
