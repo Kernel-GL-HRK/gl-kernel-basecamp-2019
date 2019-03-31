c4b8a5eaaca329917f6c3a510792fac60b063506
Kulichenko Viacheslav <Kulichenko.Viacheslav@gmail.com>
2019-03-31 17:23:18 +0300

    Lesson 2 Part 2: Add main file to archive
    Create archive file arc.tgz and add main file to archive
    
    Signed-off-by: Kulichenko Viacheslav <Kulichenko.Viacheslav@gmail.com>

--- Task2/script.sh
+++ Task2/script.sh
@@ -4,3 +4,6 @@ mkdir /tmp/guess_a_number
 
 find /home/lexx/gl-kernel-basecamp-2019/Task1 -name "*.c" -exec cp {} /tmp/guess_a_number \;
 
+cd /tmp/guess_a_number
+
+tar -cvzf /tmp/guess_a_number/arc.tgz *
