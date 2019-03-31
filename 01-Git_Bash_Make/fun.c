1289b752a08ebb5e711a9214e2fc509157c45316
Daniil Petrov <daaaanil81@gmail.com>
2019-03-31 14:06:37 +0300

    Lesson 2 Part 3: Devide first source file on three files
    Signed-off-by: Daniil Petrov <daaaanil81@gmail.com>

---
+++ 01-Git_Bash_Make/fun.c
@@ -0,0 +1,25 @@
+#include "fun.h"
+void function()
+{
+    srand(time(NULL));
+    int computer = rand() % 10;
+    int user = 0;
+    while (1) {
+        printf("Pleace, Enter number 0-9: \n");
+        printf("For exit, enter number: -1\n");
+        scanf("%d", &user);
+        if (user < -1 || user > 9){
+            printf("Number should be within of 0-9 range\n");
+            continue;
+        }
+        if (user == -1){
+            break;
+        }
+        if (user == computer){
+            printf("You Win!!!\n");
+        }
+        else{
+            printf("You Lose!!!\n");
+        }
+    }
+}
---
+++ 01-Git_Bash_Make/homework01.c
@@ -0,0 +1,8 @@
+#include "fun.h"
+
+int main()
+{
+    function();
+    printf("Goodbye\n");
+    return 0;
+}
