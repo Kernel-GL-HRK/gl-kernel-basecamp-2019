7e480e35c5df0b15de9695e011613070d3622879
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-01 03:10:31 +0300

    Lesson 3 part 2: add find fucntion Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -2,3 +2,4 @@
 Target_Path = /tmp/guesanumber/
 Home_Path = /home/vvi/part1
 Release_Path = /home/vvi/part1/release
+find $(Home_Path) -name "*.[ch]*" -exec cp {} $(Target_Path)  \; || echo "no files"
