1b109777336f8f0aacaba44f84eff362a986e630
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-01 03:20:22 +0300

    Lesson 3 part 2: add indentation at the end Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -5,3 +5,4 @@ Release_Path = /home/vvi/part1/release
 find $(Home_Path) -name "*.[ch]*" -exec cp {} $(Target_Path)  \; || echo "no files"
 tar cfz $(Target_Path)arc.tgz $(Target_Path)*
 mv $(Target_Path)arc.tgz $(Release_Path)
+
