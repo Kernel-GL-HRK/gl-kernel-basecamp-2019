4f0ee8a82d8b06a7e451e6db587c921ff04d2be2
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-01 04:33:48 +0300

    Lesson 4 part 3: add main() function Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: vvi
+ *
+ * Created on March 29, 2019, 6:10 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Second.c"
+
+/*
+ * 
+ */
+int main() {
+    Game_algorithm
+    return 0;
+}
+
+/*
+ * 
+ */
+
