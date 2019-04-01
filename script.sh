84c44322ad6da1071a3f3a935e1668c2ae1a69f3
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-01 03:14:00 +0300

    Lesson 3 part 2: add move function Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ Home_Path = /home/vvi/part1
 Release_Path = /home/vvi/part1/release
 find $(Home_Path) -name "*.[ch]*" -exec cp {} $(Target_Path)  \; || echo "no files"
 tar cfz $(Target_Path)arc.tgz $(Target_Path)*
-
+mv $(Target_Path)arc.tgz $(Release_Path)
