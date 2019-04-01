7337128d5bcda64bd752014a8467bca5961cbc9a
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-01 03:12:35 +0300

    Lesson 3 part 2: add comprassion function Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -3,3 +3,5 @@ Target_Path = /tmp/guesanumber/
 Home_Path = /home/vvi/part1
 Release_Path = /home/vvi/part1/release
 find $(Home_Path) -name "*.[ch]*" -exec cp {} $(Target_Path)  \; || echo "no files"
+tar cfz $(Target_Path)arc.tgz $(Target_Path)*
+
