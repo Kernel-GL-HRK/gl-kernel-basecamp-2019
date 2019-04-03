66629474883a86b636356b75428a4995922dd1a3
Eduard Voronkin <eduard.voronkin@nure.ua>
2019-04-03 20:32:10 +0300

    Lesson 7 Part 1: Add functions for memory allocating
    Signed-off-by: Eduard Voronkin <eduard.voronkin@nure.ua>

--- 02-KernelModules_CharacterDevice/Character_driver.c
+++ 02-KernelModules_CharacterDevice/Character_driver.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/moduleparam.h>
 #include <linux/stat.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Eduard.Voronkin<eduard.voronkin@nure.ua");
@@ -14,16 +15,43 @@ static uint BUF_SIZE = MIN_BUF_SIZE; //by default
 module_param(BUF_SIZE, uint, S_IRUGO); //Parameter may be read only by owner of the file
 MODULE_PARM_DESC(BUF_SIZE, "The size of buffer");
 
+
+static const char *driver_buffer = NULL; //Pointer to driver buffer
+static uint read_position = 0;
+static size_t data_length = 0; //Length of data in buffer
+
+static int create_buffer(void){
+    driver_buffer = kmalloc(BUF_SIZE, GFP_KERNEL);
+    if(driver_buffer == NULL)
+        return -ENOMEM;
+    return 0;
+}
+
+
+static void delete_buffer(void){
+    if(driver_buffer != NULL){
+        kfree(driver_buffer);
+        driver_buffer = NULL;
+    }
+    data_length = read_position = 0;
+}
+
+
 static int __init char_driver_init(void){
     if(BUF_SIZE < MIN_BUF_SIZE){
-        printk("Buffer size must me at least 1024 bytes.\n");
+        printk( KERN_DEBUG "Buffer size must me at least 1024 bytes.\n");
         return -1;
     }
+    if(create_buffer() < 0){
+        printk( KERN_DEBUG "Failed to allocate memory...\n");
+        return -1;
+    }
+    //Some actions will be performed here
     return 0;
-        //Some actions will be performed here
 }
 
 static void __exit char_driver_exit(void){
+    delete_buffer();
     //Some actions will be performed here
 }
 
