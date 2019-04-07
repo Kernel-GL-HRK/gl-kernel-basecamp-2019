ed590e338dd5496cb0936d3296285802e52b12ed
Mark Rubinshteyn <mark.rubinshteyn@meta.ua>
2019-04-07 16:05:09 +0300

    Create simple module part 3:Add includes to chrdev.c
    Added includes to use command-line arguments.
    Added major minor numbers and buffer size values
    
    Signed-off-by: Mark Rubinshteyn <mark.rubinshteyn@meta.ua>

---
+++ 02-KernelModules_CharacterDevice/chrdev.c
@@ -0,0 +1,32 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>  
+#include <linux/init.h>
+#include <linux/kernel.h>       
+#include <linux/slab.h>		
+#include <linux/fs.h>           
+#include <linux/types.h>        
+#include <linux/cdev.h>
+#include <asm/uaccess.h> 
+#include <linux/device.h>
+#include <linux/err.h>
+
+#define CLASS_NAME	"chrdev"
+#define DEVICE_NAME	"chrdev_example"
+
+
+static int BUFFER_SIZE=1024;
+static int major = 0; 
+static int minor = 0; 
+static int count = 1; 
+module_param(major, int, S_IRUGO);
+module_param(minor, int, S_IRUGO);
+module_param(count, int, S_IRUGO);
+module_param(BUFFER_SIZE, int, S_IRUGO);
+ 
+static struct class *pclass;
+static struct device *pdev;
+
+static int data_size;
+static unsigned char *data_buffer;
+
+static int is_open;
