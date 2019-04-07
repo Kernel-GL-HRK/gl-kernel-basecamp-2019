c579c0df3494aca2d293c78b4233a265577f100d
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-04-07 21:45:15 +0300

    Lesson 5-7 part 1: Implement /dev operations
    Implemented dev_read, dev_write, dev_open and dev_release functions.
    Added variables "is_open", "used_memory".
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- 02-KernelModules_CharacterDevice/main.c
+++ 02-KernelModules_CharacterDevice/main.c
@@ -21,6 +21,8 @@ module_param(buffer_increase, int, 0220);
 
 static int buffer_size = BUFFER_SIZE;
 static char *data_buff;
+static int is_open = 0;
+static int used_memory = 0;
 
 //===Buffer operations===
 
@@ -41,3 +43,77 @@ static void buffer_clean(void)
         data_buff = NULL;
     }
 }
+
+//===/dev file operations===
+
+int dev_open(struct inode *inode, struct file *filp)
+{	
+	if(is_open){
+		printk(KERN_ERR "ChD: Filed to open - device is busy");
+		return -EBUSY;
+	}
+	is_open = 1;
+	return 0;
+}
+
+int dev_release(struct inode *inode, struct file *filp)
+{
+	is_open = 0;
+	return 0;
+}
+
+ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+	int error = 0;
+
+	int remain = buffer_size - (int) (*offset);
+	
+	if(0 == remain)
+		return 0;
+	if(len > remain)
+		len = remain;
+	
+	error = raw_copy_to_user(buffer, data_buff, len);
+	
+	
+	if(error){
+		printk(KERN_INFO "ChD: Failed to send %d characters\n", error);
+		return -EFAULT;
+	}
+	else{
+		*offset += len;
+		return len;
+	}
+}
+
+ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
+
+	int error = 0;
+	int remain = buffer_size - (int) (*offset);
+	
+	used_memory = len;
+	
+	if(0 == remain)
+		return 0;
+	if(used_memory > remain)
+		return -EIO;
+	
+	error = raw_copy_from_user(data_buff, buffer, used_memory);
+	
+	if(error){
+		printk(KERN_INFO "ChD: Failed to receive %zu characters from the user\n", used_memory);
+		return -EFAULT;
+	}
+	else{
+		printk(KERN_INFO "ChD: Received %zu characters from the user\n", used_memory);
+		offset += used_memory;
+		return used_memory;
+
+	}
+}
+
+static struct file_operations fops = {
+	.open = dev_open,
+	.read = dev_read,
+	.write = dev_write,
+	.release = dev_release,
+};
