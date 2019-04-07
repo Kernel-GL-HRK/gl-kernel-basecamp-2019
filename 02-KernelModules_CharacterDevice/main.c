69b09f82ca8a83fb1e7117fd7e593d80c34e0cb4
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-04-07 21:06:57 +0300

    Lesson 5-7 part 1: Implement buffer operations
    Implemented buffer create and cleanup functions.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- 02-KernelModules_CharacterDevice/main.c
+++ 02-KernelModules_CharacterDevice/main.c
@@ -18,3 +18,26 @@
 int buffer_increase = 0;
 
 module_param(buffer_increase, int, 0220);
+
+static int buffer_size = BUFFER_SIZE;
+static char *data_buff;
+
+//===Buffer operations===
+
+static int buffer_create(void)
+{
+    data_buff = kmalloc(buffer_size+buffer_increase, GFP_KERNEL);
+
+    if (NULL == data_buff) 
+        return -ENOMEM;
+
+    return 0;
+}
+
+static void buffer_clean(void)
+{
+    if (data_buff) {
+        kfree(data_buff);
+        data_buff = NULL;
+    }
+}
