a8f013de4dad3dbf08cad68fcb04be0be67be217
Kvochka Nikita <yorkened@gmail.com>
2019-04-08 23:12:41 +0300

    Lesson05-07: Add kernel module description
    Added info about module license, description, author and version
    
    Signed-off-by: Kvochka Nikita <yorkened@gmail.com>

--- 02-KernelModules_CharacterDevice/src/intercom.c
+++ 02-KernelModules_CharacterDevice/src/intercom.c
@@ -13,3 +13,8 @@ static void __exit intercom_exit(void)
 
 module_init(intercom_init);
 module_exit(intercom_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nikita Kvochka <yorkened@gmail.com>");
+MODULE_DESCRIPTION("Simple char dev for text messaging between users");
+MODULE_VERSION("0.1");
