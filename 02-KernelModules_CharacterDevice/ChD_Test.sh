e575f56a6d2be0bf595c600a5ee644b9444aaa23
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-04-08 22:48:42 +0300

    Lesson 5-7 part 3: Implement testing operations
    Implemented reading/writing to device buffer, checking memory status
    through procfs, cleaning buffer through sysfs and increasing buffer size
    using module parameter.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- 02-KernelModules_CharacterDevice/ChD_Test.sh
+++ 02-KernelModules_CharacterDevice/ChD_Test.sh
@@ -10,3 +10,38 @@ Dev="/dev/charDevice"
 
 Sys="/sys/kernel/ChD/clear_memory"
 
+echo "Loading module:"
+insmod $Module_location
+
+echo "Inital memory status (Buffer size, Free memory):"
+cat $Proc
+
+echo "Attempt to write string 'Some text' to device buffer:"
+echo "Some text" > $Dev 
+
+echo "Checking memory status after using some buffer space:"
+cat $Proc
+
+echo "Reading from buffer:"
+cat $Dev
+
+echo "Cleaning device buffer through sysfs:"
+echo 1 > $Sys
+
+echo "Checking memory status after cleaning buffer:"
+cat $Proc
+
+echo "Unloading module:"
+rmmod $Module
+
+echo "Loading module with buffer_increase module parameter:"
+insmod $Module_location buffer_increase=512
+
+echo "Checking increased memory status:"
+cat $Proc
+
+echo "Unloading module:"
+rmmod $Module
+
+echo "Done"
+
