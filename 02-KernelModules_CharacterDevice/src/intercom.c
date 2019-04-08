03b272c2d3a0eb5135efe0a33b7555d7dee30624
Kvochka Nikita <yorkened@gmail.com>
2019-04-08 23:20:43 +0300

    Lesson05-07: Include needed header files
    Included headers for memory allocation, work with filesystems,
    devices, procfs, sysfs etc
    
    Signed-off-by: Kvochka Nikita <yorkened@gmail.com>

--- 02-KernelModules_CharacterDevice/src/intercom.c
+++ 02-KernelModules_CharacterDevice/src/intercom.c
@@ -1,5 +1,18 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/version.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/slab.h>
+#include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/sysfs.h>
+#include <linux/string.h>
 
 static int __init intercom_init(void)
 {
