d8a17aacfd62db6b7cbff09e835f25d47dec1fab
Kvochka Nikita <yorkened@gmail.com>
2019-04-09 11:19:57 +0300

    Lesson05-07: Add check if kernel module was loaded in system
    Signed-off-by: Kvochka Nikita <yorkened@gmail.com>

--- 02-KernelModules_CharacterDevice/test/devtest.sh
+++ 02-KernelModules_CharacterDevice/test/devtest.sh
@@ -3,3 +3,8 @@
 DEVICE=/dev/intercom
 PROCFS=/proc/intercom/bufstate
 SYSFS=/sys/class/GL/clear_buf
+
+if [[ ! -c $DEVICE || ! -f $PROCFS || ! -f $SYSFS ]]
+	then echo "Error! Module not loaded"
+	exit -1
+fi
