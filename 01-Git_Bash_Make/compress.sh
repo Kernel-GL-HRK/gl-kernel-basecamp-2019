07a06a0bffa7e68229998703f0b942f2ebf362b4
Vlad Fear <onlylastfear1@gmail.com>
2019-04-10 18:05:06 +0300

    Lesson 1: Task 2: Created subdir and copy archive to it
    Signed-off-by: Vlad Fear <onlylastfear1@gmail.com>

--- 01-Git_Bash_Make/compress.sh
+++ 01-Git_Bash_Make/compress.sh
@@ -4,3 +4,7 @@ path=$(pwd)
 cp -r . /tmp/guessanumber
 cd /tmp
 tar -zcvf guessanumber.tar.gz guessanumber/
+release_path="$path/../release"
+mkdir -p "$release_path"
+cp guessanumber.tar.gz "$release_path"
+
