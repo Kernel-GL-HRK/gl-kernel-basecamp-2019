619bb66a2926f15c2b69ad3f07699c81a589bb0a
Daniil Petrov <daaaanil81@gmail.com>
2019-04-10 07:38:25 +0300

    Lesson5-7: Create file /dev/chardev and class /sys/class/chardev
    Signed-off-by: Daniil Petrov <daaaanil81@gmail.com>

--- 02-KernelModules_CharacterDevice/moduleCharDev.c
+++ 02-KernelModules_CharacterDevice/moduleCharDev.c
@@ -11,8 +11,8 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 
-#define PROC_FILENAME "MyModule"
-#define DRIVER_NAME "MyDriver"
+#define PROC_FILENAME "MyCharDev"
+#define NAME_SYSCLASS "chardev"
 #define SIZE_STRING 10
 
 static struct proc_dir_entry *proc_file;
@@ -20,6 +20,9 @@ static dev_t first;
 static size_t countFile = 1;
 static size_t all_memory = 1024;
 static char size_memory[SIZE_STRING];
+static struct class *class_device;
+struct device *dev_ret;
+
 
 static ssize_t proc_read(struct file *f, char __user *buffer, size_t len, loff_t *off)
 {
@@ -42,7 +45,21 @@ static int __init dev_init(void) /* Constructor */
     printk(KERN_INFO "Registered: <%d %d>\n", MAJOR(first),MINOR(first));
     proc_file = proc_create(PROC_FILENAME, S_IFREG | S_IRUGO | S_IWUGO, NULL, &fops);
     if(proc_file == NULL) {
+        unregister_chrdev_region(first, countFile);
         return -ENOMEM;
+    }
+    if (IS_ERR(class_device = class_create(THIS_MODULE, NAME_SYSCLASS)))
+    {
+        remove_proc_entry(PROC_FILENAME, NULL);
+        unregister_chrdev_region(first, countFile);
+        return PTR_ERR(class_device);
+    }
+    if (IS_ERR(dev_ret = device_create(class_device, NULL, first, NULL, NAME_SYSCLASS)))
+    {
+        class_destroy(class_device);
+        remove_proc_entry(PROC_FILENAME, NULL);
+        unregister_chrdev_region(first, countFile);
+        return PTR_ERR(dev_ret);
     }
     return 0;
 }
@@ -50,8 +67,12 @@ static int __init dev_init(void) /* Constructor */
 static void __exit dev_exit(void) /* Destructor */
 {
     printk(KERN_INFO "End device");
+    
+    device_destroy(class_device, first);
+    class_destroy(class_device);
     remove_proc_entry(PROC_FILENAME, NULL);
     unregister_chrdev_region(first, countFile);
+    
 }
 
 module_init(dev_init);
