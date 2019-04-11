6973238f16f273234df337b3af9fd77fd46b4a17
Dmytro Kalynenko <dmytrokalynenko@gmail.com>
2019-04-12 00:39:05 +0300

    Lesson 5-7: Add function device creation
    Add to main file.c two function init and exit
    
    Signed-off-by: Dmytro Kalynenko <dmytrokalynenko@gmail.com>

--- Device/char_Mydriver.c
+++ Device/char_Mydriver.c
@@ -1,13 +1,33 @@
 #include <linux/module.h>	//for module_init/module_exit
+#include <linux/module.h>	//module_init/module_exit
+#include <linux/device.h>	//class, device structs
+#include <linux/types.h>	//dev_t
+#include <linux/fs.h>		//alloc_chrdev_region, unregister_chrdev_region
+
+#define CLASS_NAME "chdev_class"
+#define DEVICE_NAME "chdev"
+#define NUMBER_OF_DEVICES 1
+
+static struct class *chdev_class;
+static struct device *chdev;
+static dev_t dev_num;
 
 static int __init init(void)
 {
+	alloc_chrdev_region(&dev_num, 0, NUMBER_OF_DEVICES, DEVICE_NAME);
+	chdev_class = class_create(THIS_MODULE, CLASS_NAME);
+	chdev = device_create(chdev_class, NULL, dev_num, NULL, DEVICE_NAME);
+
 	printk("Driver loaded!\n");
 	return 0;
 }
 
 static void __exit exit(void)
 {
+	device_destroy(chdev_class, dev_num);
+	class_destroy(chdev_class);
+	unregister_chrdev_region(dev_num, NUMBER_OF_DEVICES);
+
 	printk("Driver removed!\n");
 }
 
