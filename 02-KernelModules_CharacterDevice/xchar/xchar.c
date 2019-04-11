8259b938532a98ad547863988322d4de6edae94e
xone <xone@ukr.net>
2019-04-12 01:14:01 +0300

    lesson07 part2: use sysfs for buffer clean up
    The "/sys/class/xchar/clean_up_buffere" file were created.
    Write a non-zero integer to this file to clear the buffer.
    
    Signed-off-by: xone <xone@ukr.net>

--- 02-KernelModules_CharacterDevice/xchar/xchar.c
+++ 02-KernelModules_CharacterDevice/xchar/xchar.c
@@ -11,20 +11,20 @@
 /* command-line args */
 #include <linux/moduleparam.h>
 
-#define CLASS_NAME "xchar_class"
-#define DEVICE_NAME "xchar"
+#define CLASS_NAME "xchar"
+#define DEVICE_NAME "xchar_device"
 
-#define PROC_DIRECTORY DEVICE_NAME
+#define PROC_DIRECTORY CLASS_NAME
 #define PROC_BUFFER_SIZE_FILENAME "buffer_size"
 #define PROC_USED_BUFFER_VOLUME_FILENAME "used_buffer_volume"
 #define PROC_BUFFER_SIZE 10
-
 #define PROC_PARAM_NUM 2
 #define PROC_BUFFER_SIZE_ID 0
 #define PROC_USED_BUFFER_VOLUME_ID 1
+#define SYS_BUFFER_SIZE 10
 
 /**
- *  Module parameter, eg: insmod ./xchar.ko buffer=1024
+ *  Module parameter, eg: insmod ./xchar.ko buffer_size=1024
  */
 static unsigned int buffer_size = 1024;
 module_param(buffer_size, uint, S_IRUGO);
@@ -39,6 +39,11 @@ static size_t data_size;
 static unsigned char *data_buffer = NULL;
 
 /*
+ * sysfs resources
+ */
+static char sys_buffer[SYS_BUFFER_SIZE];
+
+/*
  * procfs resources
  */
 static char proc_buffer[PROC_PARAM_NUM][PROC_BUFFER_SIZE];
@@ -50,12 +55,15 @@ static struct proc_dir_entry *proc_file[PROC_PARAM_NUM];
 
 static ssize_t proc_buffer_size_read(struct file *file_p, char __user *buffer,
 				     size_t length, loff_t *offset);
+
 static ssize_t proc_buffer_size_write(struct file *file_p,
 				      const char __user *buffer, size_t length,
 				      loff_t *offset);
+
 static ssize_t proc_used_buffer_volume_read(struct file *file_p,
 					    char __user *buffer, size_t length,
 					    loff_t *offset);
+
 static ssize_t proc_used_buffer_volume_write(struct file *file_p,
 					     const char __user *buffer,
 					     size_t length, loff_t *offset);
@@ -69,6 +77,40 @@ static struct file_operations proc_used_buffer_volume_fops = {
 	.read = proc_used_buffer_volume_read,
 	.write = proc_used_buffer_volume_write,
 };
+
+/*
+ * sysfs operations
+ */
+static ssize_t clean_up_buffer_show(struct class *class,
+				    struct class_attribute *attr, char *buf)
+{
+	sprintf(sys_buffer, "%zu", data_size);
+	strcpy(buf, sys_buffer);
+	pr_info(CLASS_NAME ": (sysfs) read %zu chars\n", strlen(buf));
+	return strlen(buf);
+}
+
+static ssize_t clean_up_buffer_store(struct class *class,
+				     struct class_attribute *attr,
+				     const char *buf, size_t count)
+{
+	int res;
+	if (SYS_BUFFER_SIZE <= count)
+		count = SYS_BUFFER_SIZE - 1;
+	strncpy(sys_buffer, buf, count);
+	sys_buffer[count] = '\0';
+	pr_info(CLASS_NAME ": (sysfs) write %zu chars\n", count);
+	if (kstrtoint(sys_buffer, 10, &res) == 0) {
+		pr_info(CLASS_NAME
+			": (sysfs) write a nonzero integer, let's clear the buffer\n");
+		data_size = 0;
+	} else {
+		pr_info(CLASS_NAME ": (sysfs) zero value is ignored\n");
+	}
+	return count;
+}
+
+CLASS_ATTR_RW(clean_up_buffer);
 
 /*
  * procfs operations
@@ -140,11 +182,11 @@ static ssize_t proc_buffer_size_read(struct file *file_p, char __user *buffer,
 		complete = 1;
 
 	if (left)
-		pr_err(DEVICE_NAME
+		pr_err(CLASS_NAME
 		       ": (procfs) failed to read %zu from %zu chars\n",
 		       left, length);
 	else
-		pr_info(DEVICE_NAME ": (procfs) read %zu chars\n", length);
+		pr_info(CLASS_NAME ": (procfs) read %zu chars\n", length);
 
 	return length - left;
 }
@@ -178,11 +220,11 @@ static ssize_t proc_used_buffer_volume_read(struct file *file_p,
 		complete = 1;
 
 	if (left)
-		pr_err(DEVICE_NAME
+		pr_err(CLASS_NAME
 		       ": (procfs) failed to read %zu from %zu chars\n",
 		       left, length);
 	else
-		pr_info(DEVICE_NAME ": (procfs) read %zu chars\n", length);
+		pr_info(CLASS_NAME ": (procfs) read %zu chars\n", length);
 
 	return length - left;
 }
@@ -191,7 +233,7 @@ static ssize_t proc_buffer_size_write(struct file *file_p,
 				      const char __user *buffer, size_t length,
 				      loff_t *offset)
 {
-	pr_info(DEVICE_NAME ": write to /procf/buffer_size is forbidden.\n");
+	pr_info(CLASS_NAME ": write to /procf/buffer_size is forbidden.\n");
 	return length;
 }
 
@@ -199,7 +241,7 @@ static ssize_t proc_used_buffer_volume_write(struct file *file_p,
 					     const char __user *buffer,
 					     size_t length, loff_t *offset)
 {
-	pr_info(DEVICE_NAME
+	pr_info(CLASS_NAME
 		": write to /proc/used_buffer_volume is forbidden.\n");
 	return length;
 }
@@ -210,19 +252,19 @@ static ssize_t proc_used_buffer_volume_write(struct file *file_p,
 static int dev_open(struct inode *inodep, struct file *filep)
 {
 	if (is_open) {
-		pr_err(DEVICE_NAME ": already open\n");
+		pr_err(CLASS_NAME ": already open\n");
 		return -EBUSY;
 	}
 
 	is_open = 1;
-	pr_info(DEVICE_NAME ": device opened\n");
+	pr_info(CLASS_NAME ": device opened\n");
 	return 0;
 }
 
 static int dev_release(struct inode *inodep, struct file *filep)
 {
 	is_open = 0;
-	pr_info(DEVICE_NAME ": device closed\n");
+	pr_info(CLASS_NAME ": device closed\n");
 	return 0;
 }
 
@@ -231,7 +273,7 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len,
 {
 	int ret;
 
-	pr_info(DEVICE_NAME ": read from file %s\n",
+	pr_info(CLASS_NAME ": read from file %s\n",
 		filep->f_path.dentry->d_iname);
 
 	if (len > data_size)
@@ -239,7 +281,7 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len,
 
 	ret = copy_to_user(buffer, data_buffer, len);
 	if (ret) {
-		pr_err(DEVICE_NAME ": copy_to_user failed: %d\n", ret);
+		pr_err(CLASS_NAME ": copy_to_user failed: %d\n", ret);
 		return -EFAULT;
 	}
 	data_size = 0; /* eof for cat */
@@ -248,7 +290,7 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len,
 	proc_msg_length[PROC_USED_BUFFER_VOLUME_ID] =
 		strlen(proc_buffer[PROC_USED_BUFFER_VOLUME_ID]);
 
-	pr_info(DEVICE_NAME ": %zd bytes read\n", len);
+	pr_info(CLASS_NAME ": %zd bytes read\n", len);
 	return len;
 }
 
@@ -257,7 +299,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len,
 {
 	int ret;
 
-	pr_info(DEVICE_NAME ": write to file %s\n",
+	pr_info(CLASS_NAME ": write to file %s\n",
 		filep->f_path.dentry->d_iname);
 
 	data_size = len;
@@ -270,11 +312,11 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len,
 
 	ret = copy_from_user(data_buffer, buffer, data_size);
 	if (ret) {
-		pr_err(DEVICE_NAME ": copy_from_user failed: %d\n", ret);
+		pr_err(CLASS_NAME ": copy_from_user failed: %d\n", ret);
 		return -EFAULT;
 	}
 
-	pr_info(DEVICE_NAME ": %zu bytes written\n", data_size);
+	pr_info(CLASS_NAME ": %zu bytes written\n", data_size);
 	return data_size;
 }
 
@@ -300,28 +342,28 @@ static int xchar_init(void)
 
 	major = register_chrdev(0, DEVICE_NAME, &fops);
 	if (major < 0) {
-		pr_err(DEVICE_NAME ": register_xchar failed: %d\n", major);
+		pr_err(CLASS_NAME ": register_xchar failed: %d\n", major);
 		return major;
 	}
-	pr_info(DEVICE_NAME ": register_xchar ok, major = %d\n", major);
+	pr_info(CLASS_NAME ": register_xchar ok, major = %d\n", major);
 
 	pclass = class_create(THIS_MODULE, CLASS_NAME);
 	if (IS_ERR(pclass)) {
 		unregister_chrdev(major, DEVICE_NAME);
-		pr_err(DEVICE_NAME ": class_create failed\n");
+		pr_err(CLASS_NAME ": class_create failed\n");
 		return PTR_ERR(pclass);
 	}
 	pclass->devnode = xchar_devnode;
-	pr_info(DEVICE_NAME ": device class created successfully\n");
+	pr_info(CLASS_NAME ": device class created successfully\n");
 
 	data_buffer = kzalloc(buffer_size * sizeof(*data_buffer), GFP_KERNEL);
 	if (!data_buffer) {
 		class_destroy(pclass);
 		unregister_chrdev(major, DEVICE_NAME);
-		pr_info(DEVICE_NAME ": buffer allocation failed\n");
+		pr_info(CLASS_NAME ": buffer allocation failed\n");
 		return -ENOMEM;
 	}
-	pr_info(DEVICE_NAME ": buffer allocated successfully with size = %u\n",
+	pr_info(CLASS_NAME ": buffer allocated successfully with size = %u\n",
 		buffer_size);
 
 	sprintf(proc_buffer[PROC_BUFFER_SIZE_ID], "%u", buffer_size);
@@ -333,30 +375,43 @@ static int xchar_init(void)
 		strlen(proc_buffer[PROC_USED_BUFFER_VOLUME_ID]);
 
 	pdev = device_create(pclass, NULL, MKDEV(major, 0), NULL,
-			     DEVICE_NAME "0");
+			     CLASS_NAME "0");
 	if (IS_ERR(pdev)) {
 		class_destroy(pclass);
 		unregister_chrdev(major, DEVICE_NAME);
-		pr_err(DEVICE_NAME ": device_create failed\n");
+		pr_err(CLASS_NAME ": device_create failed\n");
 		return PTR_ERR(pdev);
 	}
-	pr_info(DEVICE_NAME ": device node created successfully\n");
+	pr_info(CLASS_NAME ": device node created successfully\n");
 
 	err = init_proc();
 	if (err) {
 		cleanup_proc();
-		pr_err(DEVICE_NAME
-		       ": (procfs) can't create file or directory\n");
+		class_destroy(pclass);
+		unregister_chrdev(major, DEVICE_NAME);
+		pr_err(CLASS_NAME ": can't create procfs file or directory\n");
 		return err;
 	}
-	pr_info(DEVICE_NAME ": (procfs) all resources created successfully\n");
+	pr_info(CLASS_NAME ": all procfs resources created successfully\n");
 
-	pr_info(DEVICE_NAME ": module loaded\n");
+	err = class_create_file(pclass, &class_attr_clean_up_buffer);
+	if (err) {
+		cleanup_proc();
+		class_destroy(pclass);
+		unregister_chrdev(major, DEVICE_NAME);
+		pr_err(CLASS_NAME ": can't create sysfs file\n");
+		return err;
+	}
+	pr_info(CLASS_NAME ": all sysfs resources created successfully\n");
+
+	pr_info(CLASS_NAME ": module loaded\n");
 	return 0;
 }
 
 static void xchar_exit(void)
 {
+	class_remove_file(pclass, &class_attr_clean_up_buffer);
+	cleanup_proc();
 	device_destroy(pclass, MKDEV(major, 0));
 	class_destroy(pclass);
 	unregister_chrdev(major, DEVICE_NAME);
@@ -364,8 +419,7 @@ static void xchar_exit(void)
 		kfree(data_buffer);
 		data_buffer = NULL;
 	}
-	cleanup_proc();
-	pr_info(DEVICE_NAME ": module exited\n");
+	pr_info(CLASS_NAME ": module exited\n");
 }
 
 module_init(xchar_init);
