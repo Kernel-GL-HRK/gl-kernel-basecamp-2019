bcb94fd4db4c9d177443727f5246c48c0590aab1
Dima Moruha <trluxsus@gmail.com>
2019-04-11 22:23:21 +0300

    Lesson 05-07 Part 2: Implement /sys interface
    Implement sysfs for buffer clean up
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

--- 02-KernelModules_CharacterDevice/chrdev.c
+++ 02-KernelModules_CharacterDevice/chrdev.c
@@ -1,11 +1,12 @@
-#include <linux/cdev.h>		// For cdev struct
-#include <linux/device.h>	// For module class
-#include <linux/fs.h>		// Fops
-#include <linux/init.h>		// Indicates initialization and cleanup functions	
-#include <linux/module.h>	// Module init, exit functions
+#include <linux/cdev.h>			// For cdev struct
+#include <linux/device.h>		// For module class
+#include <linux/fs.h>				// Fops
+#include <linux/init.h>			// Indicates initialization and cleanup functions	
+#include <linux/module.h>		// Module init, exit functions
 #include <linux/uaccess.h>	// copy_to_user, copy_from_user functions
-#include <linux/slab.h>		// kzalloc, kfree functions
+#include <linux/slab.h>			// kzalloc, kfree functions
 #include <linux/proc_fs.h>	// For proc intertface
+#include <linux/sysfs.h>		// For sys intertface
 
 
 MODULE_AUTHOR("Dima.Moruha <trluxsus@gmail.com>");
@@ -33,27 +34,45 @@ static struct proc_dir_entry* proc_chrdev_info;
 
 static ssize_t proc_chrdev_cdev_read(struct file *file, char __user *buf, size_t count, loff_t *ppos) 
 {
-        char msg[256];
-        int retval =0;
+		char msg[256];
+		int retval =0;
 
-        if(*ppos >= strlen(msg))
+		if(*ppos >= strlen(msg))
 				return 0;
 
-        retval += sprintf(msg,"Buffer info:\n\tallocated size - %d bytes\n\tused size - %d bytes\n", BUFFER_SIZE, data_size );
+		retval += sprintf(msg,"Buffer info:\n\tallocated size - %d bytes\n\tused size - %d bytes\n", BUFFER_SIZE, data_size );
 
-        if(copy_to_user(buf, msg, retval))
-                return -EFAULT;
+		if(copy_to_user(buf, msg, retval))
+				return -EFAULT;
 
-        *ppos = retval;
-        return retval;
+		*ppos = retval;
+		return retval;
 }
 
 static struct file_operations proc_chrdev_ops = {
-        .owner = THIS_MODULE,
-        .read = proc_chrdev_cdev_read
+		.owner = THIS_MODULE,
+		.read = proc_chrdev_cdev_read
 };
 
 // eof proc implementaition--------------------------------------------------------------------
+
+// sys implementaition--------------------------------------------------------------------
+
+static int data_clear = 0;
+
+static ssize_t sysfs__chrdev_cdev_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+		sscanf(buf, "%du", &data_clear);
+		printk("chrdev: sysfs cleanup value %d\n", data_clear);
+		if (data_clear)
+				memset(data_buffer, BUFFER_SIZE, sizeof(*data_buffer));
+		return count;
+}
+
+static struct kobject *chrdev_kobject;
+static struct kobj_attribute sysfs_attribute =__ATTR(clear_memory, 0220, NULL, sysfs__chrdev_cdev_store);
+
+// eof sys implementaition--------------------------------------------------------------------
 
 static int chrdev_open(struct inode *inodep, struct file *filep)
 {
@@ -211,6 +230,7 @@ fail_alloc_buffer:
 static void __exit chrdev_exit(void)
 {
 		printk(KERN_INFO "chrdev: exit\n");
+		kobject_put(chrdev_kobject);
 		remove_proc_entry(FILE_NAME, NULL);
 		device_destroy(chrdev_class, dev);
 		class_destroy(chrdev_class);
