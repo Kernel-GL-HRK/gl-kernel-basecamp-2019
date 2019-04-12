9db4e4026eed348201e98a57d17aaa0dd0d42b8e
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-12 06:41:18 +0300

    Lesson 5-7 part 2: Add input of variables Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -7,4 +7,15 @@ test_file_path=
 mod_name=
 test_name=
 
-
+echo "input path to your module as path/to/your/module/module_name.ko" 
+read $home_path
+echo "input path to your proc file as path/to/your/file" 
+read $proc_file_path
+echo "input path to your sys file as path/to/your/file" 
+read $sys_file_path
+echo "input path to your test file as path/to/your/file" 
+read $test_file_path
+echo "input name of your module as module_name.kp" 
+read $mod_nmae
+echo "input name of your test program" 
+read $test_name
