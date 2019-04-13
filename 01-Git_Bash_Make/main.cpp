5b8d9bc33b4775950a149878eba33313e78545ec
VladFear <onlylastfear1@gmail.com>
2019-04-14 02:40:14 +0300

    Lesson1: Part3: Removed inmplementation in main.cpp
    Signed-off-by: VladFear <onlylastfear1@gmail.com>

--- 01-Git_Bash_Make/main.cpp
+++ 01-Git_Bash_Make/main.cpp
@@ -7,23 +7,5 @@ int main(int argc, char** argv)
 {
 	srand(time(NULL));
 
-	cout << "Enter value: ";
-	int inputvalue = 0;
-	cin >> inputvalue;
-
-	if (inputvalue < 0 || inputvalue > 9)
-	{
-		cout << "Input is out of range!" << endl;
-		exit(-1);
-	}
-
-	int random = rand() % 10;
-	cout << random << endl;
-
-	if (inputvalue == random)
-		cout << "You win!" << endl;
-	else
-		cout << "You lose!" << endl;
-
 	return 0;
 }
