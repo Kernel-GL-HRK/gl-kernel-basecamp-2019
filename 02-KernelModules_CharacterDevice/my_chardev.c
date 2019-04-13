bd18603c46c8be4c11168d3a677de412c8be44ec
Daniel Shami <danismi715@gmail.com>
2019-04-13 18:24:46 +0300

    Lesson 07 Character Device: Add functions for dev interface
    Signed-off-by: Daniel Shami <danismi715@gmail.com>

--- 02-KernelModules_CharacterDevice/my_chardev.c
+++ 02-KernelModules_CharacterDevice/my_chardev.c
@@ -85,6 +85,73 @@ struct class_attribute sysfs = __ATTR(cleanup, 0664, NULL, sysfs_write);
 
 /**************************************************************************************************/
 
+
+/**************************************  Interface for /dev ***************************************/
+
+static int dev_open(struct inode *i, struct file *f)
+{
+	if (is_device_open) {
+		pr_err("chardev already open\n");
+		return -EBUSY;
+	}
+	is_device_open = DEVICE_OPEN;
+    printk(KERN_INFO "chardev open\n");
+    return 0;
+}
+
+static int dev_close(struct inode *i, struct file *f)
+{
+    is_device_open = DEVICE_CLOSE;
+	printk(KERN_INFO "chardev closed\n");
+	return 0;
+}
+
+static ssize_t dev_read(struct file *file_p, char __user *buffer, size_t length, loff_t *offset)
+{
+	int retval;
+
+	int len = strlen(dev_data_file.data_buff); 
+    if( length < len ) return -EINVAL; 
+    if( *offset != 0 ) { 
+       printk( KERN_INFO "=== read return : 0\n" );  
+       return 0; 
+    } 
+    retval = copy_to_user(buffer, dev_data_file.data_buff, len);
+    if(retval){
+    	pr_err("chrdev: copy_to_user failed: %d\n", retval);
+    	return -EINVAL;
+    }
+    *offset = len; 
+    return len; 
+}
+
+static ssize_t dev_write(struct file *f, const char __user *buffer, size_t length, loff_t *offset)
+{
+   	int retval;
+
+	if (length > dev_data_file.crnt_size_buff) 
+		length = dev_data_file.crnt_size_buff;
+
+	retval = copy_from_user(&dev_data_file.data_buff[dev_data_file.size_buff - dev_data_file.crnt_size_buff], buffer, length);
+	if (retval) {
+		pr_err("chrdev: copy_from_user failed: %d\n", retval);
+		return -EFAULT;
+	}
+	dev_data_file.crnt_size_buff -= length;
+	return length;
+}
+
+static struct file_operations fops =
+{
+	.owner = THIS_MODULE,
+    .open = dev_open,
+    .release = dev_close,
+    .read = dev_read,
+    .write = dev_write
+};
+
+/**************************************************************************************************/
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Daniel.Shami <danismi715@gmail");
 MODULE_DESCRIPTION("Character device driver");                                                                                   
