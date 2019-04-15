f1079a38eaf72cdabf2c9046bce3b0123367133d
Vitaliy Izhyk <vizhyk1@gmail.com>
2019-04-15 20:31:37 +0300

    Lesson 5-7 part 2: Fixed variables and add test with module param Signed-off-by: Vitaliy Izhyk <vizhyk1@gmail.com>

--- script.sh
+++ script.sh
@@ -1,42 +1,62 @@
 !#/bin/bash
-home_path=
-proc_file_path=
-dev_file_path=
-sys_file_path=
-test_file_path=
-mod_name=
-test_name=
+echo "write path to module directory as path/to/module_directory"
+read home_path
+echo "write path to proc file as path/to/proc/file"
+read proc_file_path
+echo "write path to dev file as path/to/dev/file"
+read dev_file_path
+echo "write path to sys file as path/to/sys/file"
+read sys_file_path
+read test_file_path
+echo "write path to test file as path/to/test/file"
+echo "write module name as module_name.ko"
+read mod_name
 
-echo "input path to your module as path/to/your/module/module_name.ko" 
-read $home_path
-echo "input path to your proc file as path/to/your/file" 
-read $proc_file_path
-echo "input path to your sys file as path/to/your/file" 
-read $sys_file_path
-echo "input path to your test file as path/to/your/file" 
-read $test_file_path
-echo "input name of your module as module_name.kp" 
-read $mod_nmae
-echo "input name of your test program" 
-read $test_name
 
 echo "Loading modul"
 insmod $home_path$mod_name 
 echo "checking module status"
 cat $proc_file_path
 echo "writing to device"
-echo  "0123456789" > $dev_file_path
+echo  "0123HELLO456789" > $dev_file_path
 echo "checking module status"
 cat $proc_file_path
 echo "clearing a buffer"
 echo 1 >$sys_file_path
 echo "checking module status"
 cat $proc_file_path
-echo "writing to device\n"
-echo  "0123456789" > $dev_file_path
+echo "writing to device"
+echo  "0123HELLO456789" > $dev_file_path
 echo "checking module status"
 cat $proc_file_path
 echo "reading data from device"
-$test_file_path$test_name
-echo "unlodading buffer"
+cat /dev/chtest
+echo "unloading module"
 rmmod $mod_name
+
+echo "input Size of buffer" 
+read B_size
+
+echo $home_path$mod_name B_size=$B_size
+insmod $home_path$mod_name B_size=$B_size 
+echo "Loading modul" 
+echo "checking module status"
+cat $proc_file_path
+echo "writing to device"
+echo  "0123HELLO456789" > $dev_file_path
+echo "checking module status"
+cat $proc_file_path
+echo "clearing a buffer"
+echo 1 >$sys_file_path
+echo "checking module status"
+cat $proc_file_path
+echo "writing to device"
+echo  "0123HELLOHELLLO456789" > $dev_file_path
+echo "checking module status"
+cat $proc_file_path
+echo "reading data from device"
+cat /dev/chtest
+echo "unloading module"
+rmmod $mod_name
+
+
