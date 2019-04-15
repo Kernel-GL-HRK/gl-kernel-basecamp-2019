f4841dc89d063495a874cbc20d3d0a75ed6cec7d
Eduard Voronkin <eduard.voronkin@nure.ua>
2019-04-15 12:36:53 +0300

    Lesson 7 Part 3: Add error handling
    Signed-off-by: Eduard Voronkin <eduard.voronkin@nure.ua>

--- 02-KernelModules_CharacterDevice/character_driver.c
+++ 02-KernelModules_CharacterDevice/character_driver.c
@@ -12,6 +12,7 @@
 #include <linux/proc_fs.h>
 #include <linux/kobject.h> // for kobj and kobj_create
 #include <linux/sysfs.h> // for sysfs_create_file
+#include <linux/err.h> // for IS_ERR macro
 
 
 MODULE_LICENSE("GPL");
@@ -21,6 +22,12 @@ MODULE_VERSION("0.1");
 
 
 #define MIN_BUF_SIZE   1024
+#define NUMBER_OF_DEVICES   1
+#define DEVICE_NAME "My_character_device"
+#define PROC_DIR_NAME    "My_proc_stats"
+#define PROC_FILE_NAME   "Info"
+#define SYS_DIR_NAME    "My_sys_operations"
+#define SYS_FILE_NAME   Clear
 
 static uint BUF_SIZE = MIN_BUF_SIZE; //by default
 module_param(BUF_SIZE, uint, S_IRUGO); //Parameter may be read only by owner of the file
@@ -47,8 +54,7 @@ static void delete_buffer(void){
 }
 
 //DEVICE CREATION
-#define NUMBER_OF_DEVICES   1
-#define DEVICE_NAME "My_character_device"
+
 
 ssize_t dev_read (struct file *file_p, char __user *usr_buffer, size_t len, loff_t *offset){
    printk(KERN_DEBUG "My buff size is: %d, required length of read: %d, offset is: %lld\n", data_size, len, *offset);
@@ -118,25 +124,27 @@ static dev_t dev; // major and minor number
 
 static struct cdev my_dev;
 static struct class *my_class;
-void init(void){
-    alloc_chrdev_region(&dev, 0/*statring from minor number 0*/, NUMBER_OF_DEVICES, DEVICE_NAME); //ask kernel to allocate free major number
+static int init(void){
+    if(0 >= alloc_chrdev_region(&dev, 0/*statring from minor number 0*/, NUMBER_OF_DEVICES, DEVICE_NAME)) //ask kernel to allocate free major number
+        return -1;
     cdev_init(&my_dev, &my_fops);
-    cdev_add(&my_dev, dev, 1);
+    if(0 > cdev_add(&my_dev, dev, 1))
+        return -1;
     my_class = class_create(THIS_MODULE, "Char_driver");
+    if(IS_ERR(my_class))
+        return -1;
     device_create(my_class, NULL, dev, NULL, DEVICE_NAME );
-    
+    return 0;
     printk(KERN_DEBUG "Add dev %d -- %d\n", MAJOR(dev), MINOR(dev));
 }
 
 
 // /proc INTERFACE
-#define PROC_DIR_NAME    "My_proc_stats"
-#define PROC_FILE_NAME   "Info"
 
 ssize_t proc_read_info (struct file *file_p, char __user *user_buf, size_t len, loff_t *offset){
     char answer[100];
     if(*offset != 0)
-            return 0;
+        return 0;
     int answ_length = sprintf(answer, "Size of written data: %d\n"
                     "Size of buffer: %d\n", data_size, BUF_SIZE );
     *offset += answ_length;
@@ -164,8 +172,6 @@ static void init_proc(void)
 
 
 // /sys INTERFACE
-#define SYS_DIR_NAME    "My_sys_operations"
-#define SYS_FILE_NAME   Clear
 
 
 ssize_t store_clear(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
@@ -179,8 +185,10 @@ ssize_t store_clear(struct kobject *kobj, struct kobj_attribute *attr, const cha
 static struct  kobject *kobj_dir;
 static struct kobj_attribute file_attr = __ATTR(SYS_FILE_NAME, S_IWUSR, NULL, store_clear);
 
-void init_sys(void){
+int init_sys(void){
     kobj_dir = kobject_create_and_add(SYS_DIR_NAME, NULL);
+    if(IS_ERR(kobj_dir))
+        return -1;
     sysfs_create_file(kobj_dir, &file_attr.attr);
 }
 
@@ -188,18 +196,29 @@ void init_sys(void){
 
 
 static int __init char_driver_init(void){
+    int retval = 0;
     if(BUF_SIZE < MIN_BUF_SIZE){
         printk( KERN_DEBUG "Buffer size must me at least 1024 bytes.\n");
-        return -1;
+        retval =  -1;
+        goto out;
     }
-    if(create_buffer() < 0){
+    if(0 > create_buffer()){
         printk( KERN_DEBUG "Failed to allocate memory...\n");
-        return -1;
+        retval =  -1;
+        goto out;
     }
-    init();
+    if(-1 == init()){
+        retval =  -1;
+        goto out;
+    }
     init_proc();
-    init_sys();
-    return 0;
+    if(-1 == init_sys()){
+        printk( KERN_DEBUG "Failed to init sys...\n");
+        retval =  -1;
+        goto out;
+    }
+    out:
+        return retval;
 }
 
 static void __exit char_driver_exit(void){
