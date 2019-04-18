6ee4080684dacfb8a650d65a1e12f848f434b301
Daniel Shami <danismi715@gmail.com>
2019-04-18 16:38:46 +0300

    Lesson 07 Character Device: Fix indentation and extend proc buffer
    Signed-off-by: Daniel Shami <danismi715@gmail.com>

--- 02-KernelModules_CharacterDevice/my_chardev.c
+++ 02-KernelModules_CharacterDevice/my_chardev.c
@@ -39,7 +39,7 @@ static struct data_info{
 
 static struct proc_info{
 	int  data_length;
-	char data_buff[10];
+	char data_buff[20];
 } proc_data_file;
 
 static struct proc_dir_entry *proc_file;
@@ -50,20 +50,20 @@ static struct proc_dir_entry *proc_file;
 /************************************** Interface for /proc ***************************************/
 
 static int proc_read(struct file *file_p, char __user *buffer, size_t length, loff_t *offset)
-{ 
+{
 	int len = sprintf(proc_data_file.data_buff,"%d \n", dev_data_file.crnt_size_buff);
-    if( length < len ) return -EINVAL; 
-    if( *offset != 0 ) { 
-       return 0; 
-    } 
-    if( copy_to_user( buffer, proc_data_file.data_buff, len ) ) return -EINVAL; 
-    *offset = len; 
-    return len; 
+	if( length < len ) return -EINVAL;
+	if( *offset != 0 ){
+		return 0;
+	}
+	if( copy_to_user( buffer, proc_data_file.data_buff, len ) ) return -EINVAL;
+	*offset = len;
+	return len;
 }
 
 static struct file_operations proc_fops = {
-    .read  = proc_read,
-    .write = NULL,
+	.read  = proc_read,
+	.write = NULL
 };
 
 /**************************************************************************************************/
@@ -76,7 +76,7 @@ static ssize_t sysfs_write( struct class *class, struct class_attribute *attr, c
 	memset(dev_data_file.data_buff,0,dev_data_file.size_buff);
 	dev_data_file.crnt_size_buff = dev_data_file.size_buff;
 	printk(KERN_INFO "chardev: buffer was cleanup\n");
-    return length;
+	return length;
 } 
 
 struct class_attribute sysfs = __ATTR(cleanup, 0664, NULL, sysfs_write);
@@ -93,13 +93,13 @@ static int dev_open(struct inode *i, struct file *f)
 		return -EBUSY;
 	}
 	is_device_open = DEVICE_OPEN;
-    printk(KERN_INFO "chardev open\n");
-    return 0;
+	printk(KERN_INFO "chardev open\n");
+	return 0;
 }
 
 static int dev_close(struct inode *i, struct file *f)
 {
-    is_device_open = DEVICE_CLOSE;
+	is_device_open = DEVICE_CLOSE;
 	printk(KERN_INFO "chardev closed\n");
 	return 0;
 }
@@ -107,31 +107,28 @@ static int dev_close(struct inode *i, struct file *f)
 static ssize_t dev_read(struct file *file_p, char __user *buffer, size_t length, loff_t *offset)
 {
 	int retval;
-
-	int len = strlen(dev_data_file.data_buff); 
-    if( length < len ) return -EINVAL; 
-    if( *offset != 0 ) { 
-       printk( KERN_INFO "=== read return : 0\n" );  
-       return 0; 
-    } 
-    retval = copy_to_user(buffer, dev_data_file.data_buff, len);
-    if(retval){
-    	pr_err("chrdev: copy_to_user failed: %d\n", retval);
-    	return -EINVAL;
-    }
-    *offset = len; 
-    return len; 
+	int len = strlen(dev_data_file.data_buff);
+	if( length < len ) return -EINVAL;
+	if( *offset != 0 ){
+		printk( KERN_INFO "=== read return : 0\n" );
+		return 0;
+	}
+	retval = copy_to_user(buffer, dev_data_file.data_buff, len);
+	if(retval){
+		pr_err("chrdev: copy_to_user failed: %d\n", retval);
+		return -EINVAL;
+	}
+	*offset = len;
+	return len;
 }
 
 static ssize_t dev_write(struct file *f, const char __user *buffer, size_t length, loff_t *offset)
 {
-   	int retval;
-
-	if (length > dev_data_file.crnt_size_buff) 
+	int retval;
+	if (length > dev_data_file.crnt_size_buff)
 		length = dev_data_file.crnt_size_buff;
-
 	retval = copy_from_user(&dev_data_file.data_buff[dev_data_file.size_buff - dev_data_file.crnt_size_buff], buffer, length);
-	if (retval) {
+	if (retval){
 		pr_err("chrdev: copy_from_user failed: %d\n", retval);
 		return -EFAULT;
 	}
@@ -146,10 +143,10 @@ static ssize_t dev_write(struct file *f, const char __user *buffer, size_t lengt
 static struct file_operations fops =
 {
 	.owner = THIS_MODULE,
-    .open = dev_open,
-    .release = dev_close,
-    .read = dev_read,
-    .write = dev_write
+	.open = dev_open,
+	.release = dev_close,
+	.read = dev_read,
+	.write = dev_write
 };
 
 /**************************************************************************************************/
@@ -160,49 +157,47 @@ static struct file_operations fops =
 static int __init dev_init(void)
 {
 	int retval;
-
+	
 	/* Set dynamicly major number for device */
 	major = register_chrdev(0, DEVICE_NAME, &fops);
-	if (major < 0) {
+	if (major < 0){
 		pr_err("register_chradev failed: %d\n", major);
 		return major;
 	}
 	pr_info("chardev: register_chrdev ok, major = %d\n", major);
-	
+
 	/* Create device class */
 	pclass = class_create(THIS_MODULE, CLASS_NAME);
-	if (IS_ERR(pclass)) {
+	if (IS_ERR(pclass)){
 		unregister_chrdev(major, DEVICE_NAME);
 		pr_err("chrdev class_create failed\n");
 		return PTR_ERR(pclass);
 	}
 	printk("chardev class created successfully\n");
-	
 	retval = class_create_file(pclass, &sysfs);
 	if(retval < 0){
 		goto error;
 	}
+	
 	/* Automaticly registrate device */
 	pdev = device_create(pclass, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
-	if (IS_ERR(pdev)) {
+	if (IS_ERR(pdev)){
 		goto error;
 	}
 	printk("chardev node created successfully\n");
 	/* Create proc file */
 	proc_file = proc_create(DEVICE_NAME, S_IFREG | S_IRUGO | S_IWUGO, NULL, &proc_fops);
-    if (NULL == proc_file){
-    	printk("chardev proc_file not created\n");
-    	return -EFAULT;
-    }
-    printk("chrdev proc_file created successfully\n");
-    
+	if (NULL == proc_file){
+		printk("chardev proc_file not created\n");
+		return -EFAULT;
+	}
+	printk("chrdev proc_file created successfully\n");
 	dev_data_file.size_buff = buffer_size;
+	
 	/* Create buffer */
 	if(dev_data_file.size_buff < DEFAULT_SIZE_BUFF)
 		dev_data_file.size_buff = DEFAULT_SIZE_BUFF;
-	
 	dev_data_file.crnt_size_buff = dev_data_file.size_buff;
-
 	dev_data_file.data_buff = kmalloc(dev_data_file.size_buff * sizeof(char), GFP_KERNEL);
 	if(!dev_data_file.data_buff){
 		pr_err("chardev: allovate memory failed");
@@ -210,11 +205,9 @@ static int __init dev_init(void)
 	}
 	memset(dev_data_file.data_buff,0,dev_data_file.size_buff);
 	printk(KERN_INFO "Buffer create: %d",dev_data_file.size_buff);
-
 	printk("chardev module loaded\n");
-	
 	return 0;
-	
+
 	error:
 		device_destroy(pclass, MKDEV(major, 0));
 		class_destroy(pclass);
@@ -232,13 +225,12 @@ static void __exit dev_exit(void)
 	}
 	/*Unset and unregistrate device and its class */
 	if (proc_file){
-        remove_proc_entry(DEVICE_NAME, NULL);
-        proc_file = NULL;
-    }
+		remove_proc_entry(DEVICE_NAME, NULL);
+		proc_file = NULL;
+	}
 	device_destroy(pclass, MKDEV(major, 0));
 	class_destroy(pclass);
 	unregister_chrdev(major, DEVICE_NAME);
-
 	printk("chardev: module exited\n");
 }
 
