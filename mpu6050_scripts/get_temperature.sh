3ff8d595743cfd4ceba0490639894b84bc5a5bc9
Kvochka Nikita <yorkened@gmail.com>
2019-04-21 10:48:45 +0300

    MPU6050: Add swap of word parts and turn it to signed value if needed
    - Get value of number sign bit
    - Swap parts of word value (16 bits)
    - If number is signed, modify its value
    
    Signed-off-by: Kvochka Nikita <yorkened@gmail.com>

--- mpu6050_scripts/get_temperature.sh
+++ mpu6050_scripts/get_temperature.sh
@@ -11,5 +11,13 @@ while :
 do
 	HIGH=$(i2cget -y 0 0x68 0x41)
 	LOW=$(i2cget -y 0 0x68 0x42)
+
+	SIGN=$(( (HIGH & 0x80) >> 7 ))
+	TEMP=$(( (HIGH << 8) + LOW ))
+
+	if [ $SIGN -eq 1 ] ; then
+		TEMP=$(( -(0x10000 - TEMP) ))
+	fi
+
 	sleep 1
 done
