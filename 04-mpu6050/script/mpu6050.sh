a408ac1ec0a3258023d6ea00e82213cb3585a397
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-04-22 18:11:45 +0300

    MPU6050 Task: Implement temperature conversion
    Implemented temperature convertion from device to Fahrenheit.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- 04-mpu6050/script/mpu6050.sh
+++ 04-mpu6050/script/mpu6050.sh
@@ -6,3 +6,17 @@ then
 fi
 
 i2cset -y 0 0x68 0x6b 0
+
+while [ true ]
+do
+	tempHexH=`i2cget -y 0 0x68 0x41`
+	tempHexL=`i2cget -y 0 0x68 0x42`
+
+	temperature=$(( -(((tempHexH << 8) + tempHexL) ^ ((1 << 0x10) - 1)) ))
+	
+	fahrenheit=`bc -l <<< "scale=3; ($temperature/340 + 36.53) * 1.8 + 32"`
+    
+	echo "Fahrenheit: $fahrenheit"
+	
+	sleep 0.5
+done
