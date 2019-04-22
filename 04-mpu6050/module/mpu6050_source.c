7863f21188bbdea3763a0b5b416d8e2423b92a00
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-04-22 22:18:13 +0300

    MPU6050 Task: Define register values
    Defined needed register values and added comments.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- 04-mpu6050/module/mpu6050_source.c
+++ 04-mpu6050/module/mpu6050_source.c
@@ -5,7 +5,34 @@
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 
+// Define mpu6050 Register values
 
+#define REG_CONFIG			0x1A
+#define REG_GYRO_CONFIG		0x1B
+#define REG_ACCEL_CONFIG	0x1C
+#define REG_FIFO_EN			0x23
+#define REG_INT_PIN_CFG		0x37
+#define REG_INT_ENABLE		0x38
+#define REG_ACCEL_XOUT_H	0x3B
+#define REG_ACCEL_XOUT_L	0x3C
+#define REG_ACCEL_YOUT_H	0x3D
+#define REG_ACCEL_YOUT_L	0x3E
+#define REG_ACCEL_ZOUT_H	0x3F
+#define REG_ACCEL_ZOUT_L	0x40
+#define REG_TEMP_OUT_H		0x41
+#define REG_TEMP_OUT_L		0x42
+#define REG_GYRO_XOUT_H		0x43
+#define REG_GYRO_XOUT_L		0x44
+#define REG_GYRO_YOUT_H		0x45
+#define REG_GYRO_YOUT_L		0x46
+#define REG_GYRO_ZOUT_H		0x47
+#define REG_GYRO_ZOUT_L		0x48
+#define REG_USER_CTRL		0x6A
+#define REG_PWR_MGMT_1		0x6B
+#define REG_PWR_MGMT_2		0x6C
+#define REG_WHO_AM_I		0x75
+
+// Probe/remove device
 
 static int mpu6050_probe(struct i2c_client *drv_client, const struct i2c_device_id *id)
 {
@@ -43,6 +70,8 @@ static struct i2c_driver mpu6050_driver = {
     .remove = mpu6050_remove,
     .id_table = mpu6050_idtable,
 };
+
+// Init/exit functions
 
 static int mpu6050_init(void)
 {
