2fe8d7ff0ae20a39448a822eb045b51647d3d375
Eduard Voronkin <eduard.voronkin@nure.ua>
2019-04-24 17:56:40 +0300

    MPU6050 Part 1: Add Farenheit and Celcius output
    Signed-off-by: Eduard Voronkin <eduard.voronkin@nure.ua>

--- 04-mpu6050/scripts/temperature.sh
+++ 04-mpu6050/scripts/temperature.sh
@@ -19,3 +19,15 @@ echo "Value of WHO_AM_I register is : $who_am_i"
 echo "Turning on MPU6050"
 $(i2cset -y 0 0x68 107 0)
 
+while [ true ]
+do
+        tempH=$(i2cget -y 0 0x68 65)
+        tempL=$(i2cget -y 0 0x68 66)
+        temp_abs=$(( -(((tempH << 8)+ tempL) ^ ((1 << 0x10) - 1)) ))
+        temp_C=$(echo "scale=3; $temp_abs / 340 + 36.53" | bc)
+        temp_F=$(echo "scale=3; $temp_C * 9/5 + 32" | bc)
+        echo "Celcius: $temp_C"
+        echo "Fahrenheit: $temp_F"
+        sleep 1
+done
+
