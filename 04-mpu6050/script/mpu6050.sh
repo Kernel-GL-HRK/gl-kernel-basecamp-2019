f2ae3402387a2e653acc96b673587889f0a6e0c5
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-04-25 12:26:24 +0300

    MPU6050 Task: Fix temperature conversion
    Fixed temperature conversion. Improved script output to update output line instead of writing new.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- 04-mpu6050/script/mpu6050.sh
+++ 04-mpu6050/script/mpu6050.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-started=`i2cget -y 1 0x68 0x6b`
-started="${started: -2}"
-
-if [ $started -ne 0 ]
-then 
-	i2cset -y 1 0x68 0x6b 0
-fi
+i2cset -y 1 0x68 0x6b 0
 
 while [ true ]
 do
 	tempHexH=`i2cget -y 1 0x68 0x41`
 	tempHexL=`i2cget -y 1 0x68 0x42`
-
-	temperature=$(( -(((tempHexH << 8) + tempHexL) ^ ((1 << 0x10) - 1)) ))
+	
+	sign=$(( (tempHexH & 0x80) >> 7 ))
+	temperature=$(( (tempHexH << 8) + tempHexL ))
+	
+	if [ $sign = 1 ]
+	then
+		temperature=$(( -($temperature ^ 0xffff) ))
+	fi
 	
 	fahrenheit=`bc -l <<< "scale=3; ($temperature/340 + 36.53) * 1.8 + 32"`
     
-	echo "Fahrenheit: $fahrenheit"
-	
+	echo -n "Fahrenheit: $fahrenheit"
+	echo -en "\r"
 	sleep 0.5
 done
 
