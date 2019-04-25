2571e6b5751337de9ffbac93299c5fe8adca741b
Bogdan Shchogolev <bogdan993000@gmail.com>
2019-04-25 10:45:26 +0300

    MPU6050 Task: Remove root check
    Removed root check and replaced it with check if device already activated.
    
    Signed-off-by: Bogdan Shchogolev <bogdan993000@gmail.com>

--- 04-mpu6050/script/mpu6050.sh
+++ 04-mpu6050/script/mpu6050.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-then 
-	echo "Run as root!"
-	exit
-fi
 
-i2cset -y 0 0x68 0x6b 0
+started=`i2cget -y 1 0x68 0x6b`
+started="${started: -2}"
+
+if [ $started -ne 0 ]
+then 
+	i2cset -y 1 0x68 0x6b 0
+fi
 
 while [ true ]
 do
-	tempHexH=`i2cget -y 0 0x68 0x41`
-	tempHexL=`i2cget -y 0 0x68 0x42`
+	tempHexH=`i2cget -y 1 0x68 0x41`
+	tempHexL=`i2cget -y 1 0x68 0x42`
 
 	temperature=$(( -(((tempHexH << 8) + tempHexL) ^ ((1 << 0x10) - 1)) ))
 	
-	fahrenheit=`bc -l <<< "scale=3; ($temperature/340 + 36.53) * 1.8 + 32"`
+	fahrenheit=`bc -l <<< "scale=3; ($temperature/340 + 36.53)" * 1.8 + 32"`
     
 	echo "Fahrenheit: $fahrenheit"
 	
