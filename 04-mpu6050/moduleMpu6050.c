71d0b36389c5bb72941dce4588f1e62012463dc4
Daniil Petrov <daaaanil81@gmail.com>
2019-05-04 19:20:18 +0300

    Lesson 5: Add functions for new i2c device
    Signed-off-by: Daniil Petrov <daaaanil81@gmail.com>

--- 04-mpu6050/moduleMpu6050.c
+++ 04-mpu6050/moduleMpu6050.c
@@ -12,15 +12,51 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/sysfs.h>
+#include <linux/err.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 
+static int mpu6050_probe(struct i2c_client *drv_client, const struct i2c_device_id *id)
+{
+    dev_info(&drv_client->dev, "i2c driver probed\n");
+    //printk(KERN_INFO "mpu6050: mpu6050_probe");
+    return 0;
+}
+
+static int mpu6050_remove(struct i2c_client *drv_client)
+{
+    dev_info(&drv_client->dev, "i2c driver remove\n");
+    return 0;
+}
+
+static const struct i2c_device_id mpu6050_id[] = {
+    { "mpu6050", 0 },
+    { }
+};
+MODULE_DEVICE_TABLE(i2c, mpu6050_id);
+
+static struct i2c_driver mpu6050_i2c_driver = {
+    .driver = {
+        .name = "gl_mpu6050",
+    },
+    .probe = mpu6050_probe,
+    .remove = mpu6050_remove,
+    .id_table = mpu6050_id,
+};
 static int mpu6050_init(void)
 {
+    int ret;
+    ret = i2c_add_driver(&mpu6050_i2c_driver);
     printk(KERN_INFO "mpu6050: module loaded\n");
+    printk(KERN_INFO "mpu6050: module add\n");
     return 0;
 }
 
 static void mpu6050_exit(void)
 {
+    
+    i2c_del_driver(&mpu6050_i2c_driver);
+    printk(KERN_INFO "mpu6050: module del\n");
     printk(KERN_INFO "mpu6050: module exited\n");
 }
 
