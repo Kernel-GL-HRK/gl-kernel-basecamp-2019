357ce856646f245b9d4068e6f4979fdb3743f2c0
root <vizhyk1@gmail.com>
2019-05-06 02:57:23 +0300

    Part 1: Add check of device status
    Signed-off-by: root <vizhyk1@gmail.com>

--- mpu6050_script.sh
+++ mpu6050_script.sh
@@ -6,3 +6,15 @@ TEMP_OUT_L="0x42"
 MPU6050="0x68"
 WHO_AM_I="0x75"
 
+echo "Probing for mpu6050..."
+tmp=$(i2cget -y 1 $MPU6050 $WHO_AM_I)
+sleep 0.2
+
+
+#echo "mpu" $MPU6050
+if [ "$tmp" != "$MPU6050" ]
+	then echo "MPU6050 not found"
+else  printf 'Device answer is OK and = %#x\n' $tmp 
+fi
+
+sleep 0.2
