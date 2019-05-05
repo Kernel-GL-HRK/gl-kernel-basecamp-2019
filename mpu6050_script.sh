dc80a49ff326be00edd6c33bd751510f1a4f58da
root <vizhyk1@gmail.com>
2019-05-06 02:59:10 +0300

    Part 1: Add test sequence of Temperature in 'C and 'F
    Signed-off-by: root <vizhyk1@gmail.com>

--- mpu6050_script.sh
+++ mpu6050_script.sh
@@ -23,3 +23,25 @@ sleep 0.2
 i2cset -y 1 $MPU6050 $Power 0
 
 sleep 0.2
+
+#Run cycle polling
+for (( a = 1; a<=10; a++ ))
+do 
+#Request TEMP data
+H=$(i2cget -y 1 $MPU6050 $TEMP_OUT_H)
+sleep 0.2
+L=$(i2cget -y 1 $MPU6050 $TEMP_OUT_L)
+echo "Temp H_byte ->" $H
+echo "Temp L_byte ->"  $L
+#Convert to 
+printf -v temp '%+d' $(($H <<8 |$L))
+#convert 16 bit unsigned to signed
+let "temp = temp - 2**16"
+#Convert temperature to 'C
+let "CELS=($temp + 12420 + 170)/340"
+printf 'CELSIUM temperature is %+d\n' $CELS
+#Convert temperature to 'F
+Fahr=$(echo "scale=3; ($CELS * 9 / 5) + 32" | bc)
+printf 'Fahrenheit temperature is %.3f\n' $Fahr
+sleep 1
+done
