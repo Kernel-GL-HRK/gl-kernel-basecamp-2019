4a2f63d58ed7c8a52703408eb75fe4de3ffb655b
Dima Moruha <trluxsus@gmail.com>
2019-05-05 17:20:37 +0300

    Practise 1 MPU6050 Part 1: Wake up device
    Wake up device with test condition
    
    Signed-off-by: Dima Moruha <trluxsus@gmail.com>

--- 04-mpu6050/mpu6050_temp.sh
+++ 04-mpu6050/mpu6050_temp.sh
@@ -3,3 +3,12 @@
 PWR_MGMT_1="0x6b"
 TEMP_OUT_H="0x41"
 TEMP_OUT_L="0x42"
+
+if i2cset -y 1 0x68 $PWR_MGMT_1 0x00
+then
+    echo "Device wake up\n"
+    sleep 2
+else
+    echo "Error\n"
+    exit
+fi 
