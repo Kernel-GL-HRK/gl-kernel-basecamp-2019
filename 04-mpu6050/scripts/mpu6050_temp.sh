00bda4ec9bd7b03aab1c0a1c92f5bfb914e740c5
Alexandr Datsenko <datsenkoalexander@gmail.com>
2019-05-08 01:27:43 +0300

    Practice 1 Part 1: Added read WHO_AM_I registr
    Added variable registers. Reading data from the register WHO_AM_I
    
    Signed-off-by: Alexandr Datsenko <datsenkoalexander@gmail.com>

--- 04-mpu6050/scripts/mpu6050_temp.sh
+++ 04-mpu6050/scripts/mpu6050_temp.sh
@@ -2,6 +2,9 @@
 
 set -x #enable debug mode
 
+MPU6050_ADDR="0x68"
+WHO_AM_I_REG="0x75"
+
 if i2cdetect -y 0 | grep 68
     then
         echo "MPU6050 connected success"
@@ -9,3 +12,7 @@ if i2cdetect -y 0 | grep 68
         echo  "MPU6050 not found"
         exit 1
 fi
+
+who_am_i=$(i2cget -y 0 $MPU6050_ADDR $WHO_AM_I_REG)
+echo "Value WHO_AM_I registr in mpu6050 : $who_am_i"
+
