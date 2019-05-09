923028a397301883d97149019ddee183985eb2cc
Ihor Ryzhov <ihor.ryzhov@nure.ua>
2019-05-09 04:32:56 +0300

    Practice 1 mpu6050: Add printing termperature registers

--- 04-mpu6050/script.sh
+++ 04-mpu6050/script.sh
@@ -14,4 +14,14 @@ else printf "Who_am_I = %#x\n\n" $TEMP
 
 i2cset -y 0 $REG_MPU6050 $REG_PWR_MGMT_1 $MASK_DEVICE_RESET
 
+sleep 0.2
+
+TEMP_H=$(i2cget -y 0 $REG_MPU6050 $REG_TEMP_OUT_H)
+
+printf "TEMP_OUT_H = %#x\n" $TEMP_H
+
+TEMP_L=$(i2cget -y 0 $REG_MPU6050 $REG_TEMP_OUT_L)
+
+printf "TEMP_OUT_L = %#x\n" $TEMP_L
+
 fi
