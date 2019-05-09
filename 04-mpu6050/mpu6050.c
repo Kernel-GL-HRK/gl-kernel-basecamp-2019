2fd8c99f5d61daf6393a6f9a38f50aacac8ae591
Ihor Ryzhov <ihor.ryzhov@nure.ua>
2019-05-09 21:07:47 +0300

    Practice 1 mpu6050: Add driver skeleton
    Driver skeleton consist of two functions:
    
    1. Module entry point (mpu6050_init)
    
    This function is called when driver is loaded.
    
    Should return 0 on success (driver will be loaded) or negative error value on
    failure (driver will not be loaded).
    
    2. Module exit point (mpu6050_exit)
    
    This function is called on driver removal. No return value.
    
    Both functions should be exported using module_init and module_exit macroses.
    
    Debug information is printed using printk() function and can be read using dmesg
    program.

---
+++ 04-mpu6050/mpu6050.c
@@ -0,0 +1,26 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include "mpu6050-regs.h"
+
+static int mpu6050_init(void)
+{
+	pr_info("mpu6050: module loaded\n");
+	return 0;
+}
+
+static void mpu6050_exit(void)
+{
+	pr_info("mpu6050: module exited\n");
+}
+
+module_init(mpu6050_init);
+module_exit(mpu6050_exit);
+
+MODULE_AUTHOR("Andriy.Khulap <andriy.khulap@globallogic.com>");
+MODULE_DESCRIPTION("mpu6050 I2C acc&gyro");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
