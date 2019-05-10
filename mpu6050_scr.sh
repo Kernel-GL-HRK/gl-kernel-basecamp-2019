a553e6fd031bd2df8cd0c9c6e4f6ae0fe592bc40
Vitaly Izhyk <vizhyk1@gmail.com>
2019-05-10 03:36:38 +0300

    Part 1: Add temperature outpuy in 'C and 'F
    Signed-off-by: Vitaly Izhyk <vizhyk1@gmail.com>

--- mpu6050_scr.sh
+++ mpu6050_scr.sh
@@ -23,3 +23,25 @@ sleep 0.2
 i2cset -y 1 $MPU6050 $Power 0
 
 sleep 0.2
+
+#Run cycle polling
+for (( a = 1; a<=10; a++ ))
+do 
+#Request TEMP data
+H=$(i2cget -y 1 $MPU6050 $TEMP_OUT_H)
+sleep 0.2
+L=$(i2cget -y 1 $MPU6050 $TEMP_OUT_L)
+echo "Temp H_byte ->" $H
+echo "Temp L_byte ->"  $L
+#Convert to 
+printf -v temp '%+d' $(($H <<8 |$L))
+#convert 16 bit unsigned to signed
+temp_converted=$(((temp & 0x7fff)- (temp & 0x8000)))
+#Convert temperature to 'C
+CELS=$(echo "scale = 3; $temp_converted /340 +36.53" |bc)
+printf 'CELSIUM temperature is %.3f\n' $CELS
+#Convert temperature to 'F
+Fahr=$(echo "scale=3; ($CELS * 9 / 5) + 32" | bc)
+printf 'Fahrenheit temperature is %.3f\n' $Fahr
+sleep 1
+done
