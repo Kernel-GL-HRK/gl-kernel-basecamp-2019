f871dc7d82b8e3c16e41c8244e1380ce91f73403
Mark Rubinshteyn <mark.rubinshteyn@meta.ua>
2019-05-25 10:14:41 +0300

    Thread task part 3 : Implements task
    Added master and workers threads to implement task
    
    Signed-off-by: Mark Rubinshteyn <mark.rubinshteyn@meta.ua>

--- 07-Concurrency_Synchronization/threads.c
+++ 07-Concurrency_Synchronization/threads.c
@@ -4,12 +4,17 @@
 #include <linux/slab.h>
 #include <linux/sched.h>
 #include <linux/uaccess.h>
+#include <linux/string.h>
+#include <linux/kthread.h>
+#include <linux/mutex.h>
+#include <linux/semaphore.h>
+#include <linux/delay.h>
 
 
-
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 512
 #define PROC_DIR "threads"
 #define PROC_FILENAME "Master"
+#define POOL_SIZE 3
 
 static char *buff;
 
@@ -17,6 +22,110 @@ static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_file;
 static size_t size = 0;
 static struct file_operations proc_fops;
+
+static struct semaphore workersS;
+static struct mutex lock;
+static struct mutex count_lock;
+static struct mutex task_lock;
+static struct task_struct *masterT;
+static struct task_struct *workersT[POOL_SIZE];
+static struct completion data;
+
+static char *work;
+static int stop = 0;
+static int i = POOL_SIZE;
+
+static void output(char symb, u32 len, u32 count)
+{
+        u32 i;
+        u32 j;
+
+        pr_info("thread : worker thread pid %d", current->pid);
+
+        for (i = 0; i < count; ++i) {
+                pr_info("thread : block %d: ", i + 1);
+                for (j = 0; j < len; ++j) {
+                        pr_info("%c", symb);
+                }
+                pr_info("\n");
+        }
+
+        up(&workersS);
+        ++i;
+}
+
+static int implement(void *task)
+{
+        char symb;
+        u64 len;
+        u64 count;
+        char temp[5];
+
+        strncpy(&symb, (char*)task, 1);
+
+        strncpy(temp, (char*)task + 2, 1);
+        len = simple_strtol(temp, NULL, 10);
+
+        strncpy(temp, (char*)task + 4, 1);
+        count = simple_strtol(temp, NULL, 10);
+
+        output(symb, len, count);
+
+        return 0;
+}
+
+static int worker(void *args)
+{
+        mutex_lock(&task_lock);
+
+        work = (char*)args;
+        implement(work);
+
+        mutex_unlock(&task_lock);
+
+        return 0;
+}
+
+static int master(void *args)
+{
+        char task[6];
+        pr_info("thread: master thread init\n");
+
+        while (!kthread_should_stop()) {
+                wait_for_completion(&data);
+                reinit_completion(&data);
+                if (stop) {
+                        break;
+                }
+                pr_info("thread : thread data received\n");
+
+                mutex_lock(&lock);
+                strncpy(task, buff, 6);
+                mutex_unlock(&lock);
+
+                pr_info("thread manager: master thread calling worker\n");
+                down(&workersS);
+                workersT[i] = kthread_run(worker, task, "worker_thread");
+                mutex_lock(&count_lock);
+                --i;
+                mutex_unlock(&count_lock);
+
+                pr_info("thread : data proccessed\n");
+        }
+
+        return 0;
+}
+
+static void kthreads_stop(void)
+{
+        stop = 1;
+
+        complete(&data);
+        kthread_stop(masterT);
+
+        pr_info("thread : thread stopped\n");
+}
+
 
 static void buffer_clean(void)
 {
@@ -94,8 +203,11 @@ static int proc_write(struct file *filp, const char *user_buffer, size_t count, 
         if (size > BUFFER_SIZE) {
                 size = BUFFER_SIZE;
         }
-
-        if (raw_copy_from_user(buff, user_buffer, size)) {
+	mutex_lock(&lock);
+	ret = raw_copy_from_user(buff,user_buffer,size);
+	complete(&data);
+	mutex_unlock(&lock);
+        if (ret) {
                 ret = -EFAULT;
                 goto out;
         }
@@ -122,8 +234,10 @@ static int proc_read(struct file *filp, char *buffer, size_t len, loff_t *offp)
         if (len > strlen(msg) - *offp) {
                 len = strlen(msg) - *offp;
         }
-
+	
+	mutex_lock(&lock);
         ret = raw_copy_to_user((void*)buffer, msg - *offp, len);
+	mutex_unlock(&lock);
 
         *offp += len;
 
@@ -135,7 +249,8 @@ out:
 
 void __exit thread_exit(void)
 {
-      
+
+	kthreads_stop();
         remove_proc();
 
         buffer_clean();
@@ -145,17 +260,38 @@ void __exit thread_exit(void)
 
 int __init thread_init(void)
 {
-    
-        buffer_create();
+	
+	int ret;
 
-        proc_fops.read = proc_read;
-        proc_fops.write = proc_write;
+	ret = buffer_create();
+	if (ret) {
+		goto out;
+	}
 
-        create_proc();
+	proc_fops.read = proc_read;
+	proc_fops.write = proc_write;
 
-        pr_info("thread : module loaded\n");
+	create_proc();
+	ret = create_proc();
+	if (ret) {
+		goto out;
+        }
+
+	sema_init(&workersS, POOL_SIZE);
+	pr_info("thread : semaphore init\n");
+
+	mutex_init(&lock);
+
+	init_completion(&data);
+
+	masterT = kthread_run(master, NULL, "master_thread");
+
+	pr_info("thread : module loaded\n");
 
         return 0;
+
+out:
+        return ret;
 }
 
 MODULE_LICENSE("GPL");
